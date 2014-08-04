bf872999a455a5d2426302039f151f5122b5e989
Florian Delezenne <del.florian@gmail.com>
2014-08-04 10:29:06 +0200

    Questions plus precises sur checkboxes

--- app/scripts/controllers/contexte/situations_urgentes/urgences.js
+++ app/scripts/controllers/contexte/situations_urgentes/urgences.js
@@ -33,34 +33,39 @@ angular.module('impactApp')
           labelRep: $scope.getPronoun(true) + ' n\'arrive plus à vivre à domicile',
           model: 'domicile',
           detail: true,
-          placeholder: 'Expliquer la difficulté'
+          detailUrl: 'views/partials/form_precisez_big.html',
+          placeholder: 'Expliquez la difficulté'
         },
         {
           label: 'Votre établissement ne peux plus vous acceuillir et vous ne pouvez pas retourner chez vous',
           labelRep: 'Son établissement ne peux plus l\'acceuillir et ' + $scope.getPronoun() + ' ne peut pas retourner chez ' + $scope.getPronoun(),
           model: 'etablissement',
           detail: true,
-          placeholder: 'Expliquer la difficulté'
+          detailUrl: 'views/partials/form_precisez_big.html',
+          placeholder: 'Expliquez la difficulté'
         },
         {
           label: 'Votre école ne peux plus vous acceuillir',
           labelRep: 'Son école ne peux plus l\'acceuillir',
           detail: true,
-          placeholder: 'Expliquer la difficulté',
+          detailUrl: 'views/partials/form_precisez_big.html',
+          placeholder: 'Expliquez la difficulté',
           model: 'ecole'
         },
         {
           label: 'Vous risquez de perdre votre travail',
           labelRep: $scope.getPronoun(true) + ' risque de perdre son travail',
           detail: true,
-          placeholder: 'Expliquer la difficulté',
+          detailUrl: 'views/partials/form_precisez_big.html',
+          placeholder: 'Expliquez la difficulté',
           model: 'travail'
         },
         {
           label: 'Vous commencez bientôt une nouvelle formation',
           labelRep: $scope.getPronoun(true) + ' commence bientôt une nouvelle formation',
           detail: true,
-          placeholder: 'Date d\'entrée prévue',
+          detailUrl: 'views/partials/form_precisez_date.html',
+          detailLabel: 'Date d\'entrée prévue',
           model: 'formation'
         }
       ]
