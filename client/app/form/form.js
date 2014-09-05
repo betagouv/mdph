9082279c388d8036f17f346a8b5f1aa31a2b7dc1
Florian Delezenne <del.florian@gmail.com>
2014-09-05 11:36:28 +0200

    Fix check formulaire

--- client/app/form/form.js
+++ client/app/form/form.js
@@ -11,8 +11,8 @@ angular.module('impactApp')
         resolve: {
           currentForm:  function($http) {
             return $http.get('/api/forms/mine').then(function(result) {
-              return result.data;
-            }).catch(function(err) {
+              return result.data === '404' ? undefined : result.data;
+            }).catch(function() {
               return undefined;
             });
            }
