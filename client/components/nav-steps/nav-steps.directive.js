732b8babc0f740751a6b79e55bc0065fb1f6b103
Florian Delezenne <del.florian@gmail.com>
2014-09-22 15:10:44 +0200

    Directive etape suivante/precedente

---
+++ client/components/nav-steps/nav-steps.directive.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('impactApp')
+  .directive('navSteps', function () {
+    return {
+      templateUrl: 'components/nav-steps/nav-steps.html',
+      restrict: 'EA',
+      link: function (scope, element, attrs) {
+      }
+    };
+  });
