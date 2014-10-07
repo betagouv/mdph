e516f3802c19e5ab5a78296902b759127bff8c35
Florian Delezenne <del.florian@gmail.com>
2014-10-07 12:04:08 +0200

    fix seed mdph

--- server/config/seed.js
+++ server/config/seed.js
@@ -61,100 +61,95 @@ Mdph.find({}).remove(function() {
       Form.create({
         user: bob,
         formAnswers: {
-          "contexte":{
-            "estRepresentant":true,
-            "demandeur":{
-               "prenom":"Bobby",
-               "sexe":"masculin"
+          'contexte': {
+            'estRepresentant':true,
+            'demandeur': {
+               'prenom':'Bobby',
+               'sexe':'masculin'
             },
-            "mdph":{
-               "_id":"542a6da264487ac40501ab27",
-               "name":"Calvados",
-               "zipcode":"14",
-               "__v":0
+            'mdph': mdphCalvados,
+            'nouveauDossier':false,
+            'numDossier':true,
+            'numeroDossier':'21',
+            'raison': {
+               'finDeVosDroits':true
             },
-            "nouveauDossier":false,
-            "numDossier":true,
-            "numeroDossier":"21",
-            "raison":{
-               "finDeVosDroits":true
+            'connaisTaux':true,
+            'tauxIncapacite':79,
+            'contestationTaux':'stable',
+            'dateNaissance':'1981-05-12T22:00:00.000Z',
+            'urgences': {
+               'domicile':false,
+               'formation':true
             },
-            "connaisTaux":true,
-            "tauxIncapacite":79,
-            "contestationTaux":"stable",
-            "dateNaissance":"1981-05-12T22:00:00.000Z",
-            "urgences":{
-               "domicile":false,
-               "formation":true
-            },
-            "formationDetail":"2014-10-21T22:00:00.000Z"
+            'formationDetail':'2014-10-21T22:00:00.000Z'
           },
-          "vieQuotidienne":{
-            "famille":"parents",
-            "logement":"independant",
-            "logement_independant":"proprietaire",
-            "mesPrestations":[
+          'vieQuotidienne': {
+            'famille':'parents',
+            'logement':'independant',
+            'logement_independant':'proprietaire',
+            'mesPrestations':[
                {
-                  "id":"aah",
-                  "label":"AAH",
-                  "date":"2014-09-08T22:00:00.000Z",
-                  "type":"presta-finances",
-                  "description":"L'allocation aux adultes handicapés (AAH) est versée, sous conditions de ressources, aux adultes déclarés handicapés afin de leur assurer un revenu minimum."
+                  'id':'aah',
+                  'label':'AAH',
+                  'date':'2014-09-08T22:00:00.000Z',
+                  'type':'presta-finances',
+                  'description':'L\'allocation aux adultes handicapés (AAH) est versée, sous conditions de ressources, aux adultes déclarés handicapés afin de leur assurer un revenu minimum.'
                }
             ],
-            "besoinsVie":{
-               "courses":true,
-               "habits":true,
-               "budget":true,
-               "courant":true,
-               "repas":true,
-               "menage":true
+            'besoinsVie': {
+               'courses':true,
+               'habits':true,
+               'budget':true,
+               'courant':true,
+               'repas':true,
+               'menage':true
             },
-            "besoinsDeplacement":{
-               "intraDomicile":true,
-               "public":true,
-               "transports":true,
-               "vacances":true
+            'besoinsDeplacement': {
+               'intraDomicile':true,
+               'public':true,
+               'transports':true,
+               'vacances':true
             },
-            "besoinsSocial":{
-               "communication":true,
-               "proches":true,
-               "securite":true,
-               "citoyen":true
+            'besoinsSocial': {
+               'communication':true,
+               'proches':true,
+               'securite':true,
+               'citoyen':true
             },
-            "besoinsLieuDeVie":{
-               "materiel":true,
-               "conduite":true
+            'besoinsLieuDeVie': {
+               'materiel':true,
+               'conduite':true
             },
-            "attentesTypeAide":{
-               "domicile":true,
-               "amenagement":true,
-               "financierHandicap":true,
-               "mobilite":true,
-               "etablissement":true,
-               "materiel":true
+            'attentesTypeAide': {
+               'domicile':true,
+               'amenagement':true,
+               'financierHandicap':true,
+               'mobilite':true,
+               'etablissement':true,
+               'materiel':true
             },
-            "structures":{
-               "valeur":false,
-               "structures":[
+            'structures': {
+               'valeur':false,
+               'structures':[
                   {
-                     "name":"",
-                     "contact":false
+                     'name':'',
+                     'contact':false
                   }
                ]
             },
-            "autresRenseignements":"",
-            "objetDemande":{
-               "travail":false
+            'autresRenseignements':'',
+            'objetDemande': {
+               'travail':false
             }
           },
-          "aidant":{
-            "sectionLabel":"Aidant familial",
-            "answers":{
-               "condition":false
+          'aidant': {
+            'sectionLabel':'Aidant familial',
+            'answers': {
+               'condition':false
             }
           },
-          "envoi":true
+          'envoi':true
         },
         updatedAt: new Date(),
         steps: [
