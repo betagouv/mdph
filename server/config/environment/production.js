7e7658e992f2ef91fe8b0803f55c1e92014ef623
Florian Delezenne <del.florian@gmail.com>
2014-10-17 14:03:14 +0200

    Prod: plus de 404 louche plus de seed, fix template login

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -21,8 +21,6 @@ module.exports = {
             process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
             'mongodb://localhost/impact'
   },
-
-  seedDB: true,
   
   uploadDir: 'client/assets/uploads'
 };
