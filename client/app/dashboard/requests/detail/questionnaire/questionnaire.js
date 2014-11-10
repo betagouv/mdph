6c5a1b7063203abe83cd901a621eee858be81a6e
Florian Delezenne <del.florian@gmail.com>
2014-11-10 14:33:04 +0100

    Fix quitus dans dashboard

--- client/app/dashboard/requests/detail/questionnaire/questionnaire.js
+++ client/app/dashboard/requests/detail/questionnaire/questionnaire.js
@@ -6,8 +6,15 @@ angular.module('impactApp')
       .state('dashboard.requests.detail.questionnaire', {
         url: '/questionnaire',
         templateUrl: 'app/dashboard/requests/detail/questionnaire/questionnaire.html',
-        controller: function($scope, DroitService) {
-          $scope.prestations = DroitService.compute($scope.request.formAnswers);
+        controller: function($scope, DroitService, prestations) {
+          $scope.prestations = DroitService.compute($scope.request.formAnswers, prestations);
+        },
+        resolve: {
+          prestations: function($http) {
+            return $http.get('/api/prestations').then(function(prestations) {
+              return prestations.data;
+            });
+          }
         }
       });
   });
