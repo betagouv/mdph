22997f8b8cd27fbd65ed72772028f4c08eb1edd5
Roxanne Ricci <rricci@octo.com>
2014-12-24 15:16:30 +0100

    Mise en page basique de l'évaluation + filtre de calcul de l'age

--- client/app/dashboard/repartition_demandes/evaluation/evaluation.controller.js
+++ client/app/dashboard/repartition_demandes/evaluation/evaluation.controller.js
@@ -1,6 +1,18 @@
 'use strict';
 
 angular.module('impactApp')
+  .filter('age', function() {
+     function calculAge(dateNaiss) {
+         var ageDiff = Date.now() - Date.parse(dateNaiss);
+         var ageDate = new Date(ageDiff);
+         return Math.abs(ageDate.getUTCFullYear() - 1970);
+     }
+     return function(dateNaiss) {
+           return calculAge(dateNaiss);
+     };
+});
+
+angular.module('impactApp')
   .controller('EvaluationDemandeCtrl', function () {
 
   });
