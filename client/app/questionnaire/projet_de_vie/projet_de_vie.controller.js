15dacc2620fa7e842050631ab260bf2e2e15ce96
Florian Delezenne <del.florian@gmail.com>
2015-01-06 16:36:18 +0100

    fix renouvellement

--- client/app/questionnaire/projet_de_vie/projet_de_vie.controller.js
+++ client/app/questionnaire/projet_de_vie/projet_de_vie.controller.js
@@ -4,7 +4,7 @@ angular.module('impactApp')
   .controller('ProjetDeVieCtrl', function ($scope, $state, datepickerConfig, QuestionService, $window, SectionConstants) {
     datepickerConfig.showWeeks = false;
 
-    $scope.sections = $scope.formAnswers.renouvellement ? SectionConstants : _.where(SectionConstants, {'renew': undefined});
+    $scope.sections = $scope.currentRequest.renouvellement ? SectionConstants : _.where(SectionConstants, {'renew': undefined});
 
     $scope.encode = function(json) {
       return encodeURIComponent(JSON.stringify(json));
