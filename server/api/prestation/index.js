a2c3f9720f472697d5c1d8fd1aab90a66ee43118
Florian Delezenne <del.florian@gmail.com>
2015-02-05 09:56:14 +0100

    Fix typo

--- server/api/prestation/index.js
+++ server/api/prestation/index.js
@@ -6,6 +6,6 @@ var controller = require('./prestation.controller');
 var router = express.Router();
 
 router.get('/', controller.index);
-router.post('/simulation', controller.simalate);
+router.post('/simulation', controller.simulate);
 
 module.exports = router;
