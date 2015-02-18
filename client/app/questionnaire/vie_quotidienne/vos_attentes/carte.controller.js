1f029f3c952697058511b873025070ff5d1062cd
Florian Delezenne <del.florian@gmail.com>
2015-02-18 22:29:05 +0100

    Fix questions radio, pas besoin d'init le model

--- client/app/questionnaire/vie_quotidienne/vos_attentes/carte.controller.js
+++ client/app/questionnaire/vie_quotidienne/vos_attentes/carte.controller.js
@@ -3,10 +3,6 @@
 angular.module('impactApp')
   .controller('CarteCtrl', function ($scope, $state, QuestionService) {
 
-    if (angular.isUndefined($scope.sectionModel.attentesCarte)) {
-      $scope.sectionModel.attentesCarte = {};
-    }
-
     $scope.question = QuestionService.get('vieQuotidienne', 'attentesCarte', $scope.formAnswers);
 
     $scope.nextStep = function() {
--- client/app/questionnaire/vie_quotidienne/vos_besoins/transport.controller.js
+++ client/app/questionnaire/vie_quotidienne/vos_besoins/transport.controller.js
@@ -3,10 +3,6 @@
 angular.module('impactApp')
   .controller('TransportCtrl', function($scope, $state, QuestionService) {
 
-    if (angular.isUndefined($scope.sectionModel.besoinsTransports)) {
-      $scope.sectionModel.besoinsTransports = {};
-    }
-
     $scope.question = QuestionService.get('vieQuotidienne', 'besoinsTransports', $scope.formAnswers);
 
     $scope.nextStep = function() {
