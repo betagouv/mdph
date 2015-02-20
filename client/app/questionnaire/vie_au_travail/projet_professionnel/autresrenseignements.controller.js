d222a738abbedcb4c3bfb0fd20ed937a8679f9d3
Florian Delezenne <del.florian@gmail.com>
2015-02-20 10:20:33 +0100

    Fix validation vie au travail

--- client/app/questionnaire/vie_au_travail/projet_professionnel/autresrenseignements.controller.js
+++ client/app/questionnaire/vie_au_travail/projet_professionnel/autresrenseignements.controller.js
@@ -8,12 +8,12 @@
  * Controller of the impactApp
  */
 angular.module('impactApp')
-  .controller('AutresRenseignementsProjetProCtrl', function ($scope, $state, QuestionService) {
+  .controller('AutresRenseignementsProjetProCtrl', function ($scope, $state, QuestionService, saveSection) {
 
     $scope.question = QuestionService.get('travail', 'autresRenseignements', $scope.formAnswers);
     $scope.isLastQuestion = true;
 
     $scope.nextStep = function() {
-      $scope.$parent.saveSection($scope.sectionModel);
+      saveSection();
     };
   });
