b94ac243ad04c0cb39848407ad2832a726e32b4e
Roxanne Ricci <rricci@octo.com>
2015-03-10 11:05:46 +0100

    Ajout bouton valider 'Identités' - sans vérification

--- client/app/demande/section/identites/identites.controller.js
+++ client/app/demande/section/identites/identites.controller.js
@@ -1,9 +1,10 @@
 'use strict';
 
 angular.module('impactApp')
-  .controller('IdentitesCtrl', function($scope, request, section, sectionModel, estMineur) {
+  .controller('IdentitesCtrl', function($scope, request, section, sectionModel, estMineur, saveSection) {
     $scope.request = request;
     $scope.section = section;
     $scope.estMineur = estMineur;
     $scope.sectionModel = sectionModel;
+    $scope.saveSection = saveSection;
   });
