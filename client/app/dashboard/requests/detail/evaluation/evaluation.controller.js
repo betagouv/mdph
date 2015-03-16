acfcf27cb57a86f960005c7e432dcc07a32f1518
Roxanne Ricci <rricci@octo.com>
2015-03-16 17:36:13 +0100

    Début formulaire de synthèse

--- client/app/dashboard/requests/detail/evaluation/evaluation.controller.js
+++ client/app/dashboard/requests/detail/evaluation/evaluation.controller.js
@@ -4,4 +4,34 @@ angular.module('impactApp')
   .controller('RequestEvaluationCtrl', function ($scope, sections, GevaService) {
     $scope.sections = sections;
     $scope.computeCompletion = GevaService.computeCompletion;
+    $scope.prestaDemande = [
+      {
+        label: '',
+        eligibilite: '',
+        motivation: ''
+      }
+    ];
+    $scope.prestaAutre = [
+      {
+        label: '',
+        motivation: ''
+      }
+    ];
+    $scope.preconisations = '';
+
+    $scope.ajouterPrestaDemande = function(){
+      $scope.prestaDemande.push({
+        label: '',
+        eligibilite: '',
+        motivation: ''
+      });
+    };
+
+    $scope.ajouterPrestaAutre = function(){
+      $scope.prestaAutre.push({
+        label: '',
+        motivation: ''
+      });
+    };
+
   });
