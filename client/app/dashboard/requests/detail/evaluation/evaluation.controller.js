6297efcfd8a3871558e8169ef8aae2f8ac373da8
Florian Delezenne <del.florian@gmail.com>
2015-03-16 17:45:06 +0100

    Synthese modale

--- client/app/dashboard/requests/detail/evaluation/evaluation.controller.js
+++ client/app/dashboard/requests/detail/evaluation/evaluation.controller.js
@@ -1,9 +1,24 @@
 'use strict';
 
 angular.module('impactApp')
-  .controller('RequestEvaluationCtrl', function ($scope, sections, GevaService) {
+  .controller('RequestEvaluationCtrl', function ($scope, $modal, sections, GevaService) {
     $scope.sections = sections;
     $scope.computeCompletion = GevaService.computeCompletion;
+
+    $scope.afficherSynthese = function () {
+      $modal.open({
+        templateUrl: 'app/dashboard/requests/detail/evaluation/synthese.html',
+        controller: 'ModalSyntheseCtrl',
+        resolve: {
+          sections: function () {
+            return $scope.sections;
+          }
+        }
+      });
+    };
+  })
+  .controller('ModalSyntheseCtrl', function ($scope, $modalInstance, sections) {
+    $scope.sections = sections;
     $scope.prestaDemande = [
       {
         label: '',
@@ -27,6 +42,11 @@ angular.module('impactApp')
       });
     };
 
+    $scope.retirerPresta = function(type){
+      var lastIndex = type.indexOf(_.last(type));
+      type.splice(lastIndex, 1);
+    };
+
     $scope.ajouterPrestaAutre = function(){
       $scope.prestaAutre.push({
         label: '',
@@ -34,9 +54,7 @@ angular.module('impactApp')
       });
     };
 
-    $scope.retirerPresta = function(type){
-      var lastIndex = type.indexOf(_.last(type));
-      type.splice(lastIndex, 1);
+    $scope.ok = function() {
+      $modalInstance.close();
     };
-
   });
