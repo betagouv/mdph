1b80e51930152a61e4069540985cd4b7ea2dfa13
Roxanne Ricci <rricci@octo.com>
2015-03-20 13:39:37 +0100

    Ajout message d'erreur + vérification remplissage 'Identités'

--- client/app/demande/section/identites/identites.controller.js
+++ client/app/demande/section/identites/identites.controller.js
@@ -6,5 +6,27 @@ angular.module('impactApp')
     $scope.section = section;
     $scope.estMineur = estMineur;
     $scope.sectionModel = sectionModel;
-    $scope.saveSection = saveSection;
+    $scope.error = {
+      show: false,
+      message: ''
+    };
+
+    $scope.checkIdentities = function(){
+      var formIdentites = request.formAnswers.identites;
+      if (!formIdentites.beneficiaire || !formIdentites.beneficiaire.nom){
+        $scope.error.show = true;
+        $scope.error.message = 'Veuillez renseigner un bénéficiaire.';
+      }
+      else {
+        if (estMineur(formIdentites.beneficiaire.dateNaissance)){
+          if (!formIdentites.autorite || !formIdentites.autorite.parent1 || !formIdentites.autorite.parent1 ||!formIdentites.autorite.parent1.nom|| !formIdentites.autorite.parent2 || !formIdentites.autorite.parent2.nom){
+            $scope.error.show = true;
+            $scope.error.message = 'Veuillez renseigner deux autorités parentales pour un bénéficiaire mineur.';
+          }
+        }
+        else {
+          saveSection();
+        }
+      }
+    };
   });
