3d5ee9841baa0d4319a3366c89380e4147a5c0ea
Florian Delezenne <del.florian@gmail.com>
2015-06-10 15:20:29 +0200

    Ignore angular-chart.js on rev

--- grunt-tasks/rev.js
+++ grunt-tasks/rev.js
@@ -6,7 +6,8 @@ module.exports = {
         '<%= app.dirs.dist %>/{,*/}*.css',
         '<%= app.dirs.dist %>/assets/fonts/*',
         '!<%= app.dirs.dist %>/server/*',
-        '!<%= app.dirs.dist %>/bower_components/Chart.js'
+        '!<%= app.dirs.dist %>/bower_components/Chart.js',
+        '!<%= app.dirs.dist %>/bower_components/angular-chart.js'
       ]
     }
   }
