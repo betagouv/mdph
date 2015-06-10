84eb4b7e76d6f8049a3a9d4cdba63851ea3afad5
Florian Delezenne <del.florian@gmail.com>
2015-06-10 15:19:37 +0200

    Ignore chart.js on rev

--- grunt-tasks/rev.js
+++ grunt-tasks/rev.js
@@ -5,7 +5,8 @@ module.exports = {
         '<%= app.dirs.dist %>/{,*/}*.js',
         '<%= app.dirs.dist %>/{,*/}*.css',
         '<%= app.dirs.dist %>/assets/fonts/*',
-        '!<%= app.dirs.dist %>/server/*'
+        '!<%= app.dirs.dist %>/server/*',
+        '!<%= app.dirs.dist %>/bower_components/Chart.js'
       ]
     }
   }
