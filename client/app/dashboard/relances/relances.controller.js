b59161b74ef8f42d17fd9a42b139b53d405c15d6
Florian Delezenne <del.florian@gmail.com>
2015-11-13 11:06:38 +0100

    Removing unused code

--- client/app/dashboard/relances/relances.controller.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('impactApp')
-  .controller('RelancesCtrl', function($scope, $http, $state, RequestStepService, RequestService, requestSteps, requests) {
-    $scope.requests = _.sortBy(requests, $scope.updatedAt);
-  });
