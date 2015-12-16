61663de9f7214a61e8d30904803481e7402a8b06
Florian Delezenne <del.florian@gmail.com>
2015-12-16 17:20:59 +0100

    Fix documents display

--- client/app/demande/steps/documents/obligatoires/obligatoires.controller.js
+++ client/app/demande/steps/documents/obligatoires/obligatoires.controller.js
@@ -1,11 +1,10 @@
 'use strict';
 
 angular.module('impactApp')
-  .controller('DocumentsObligatoiresCtrl', function($scope, $modal, $state, UploadService, request, DocumentsService, documentTypes) {
+  .controller('DocumentsObligatoiresCtrl', function($scope, $modal, $state, UploadService, request, documentTypes) {
     $scope.request = request;
     $scope.documentTypes = documentTypes;
-
-    $scope.filesVM = DocumentsService.groupByType(request.documents);
+    $scope.filesVM = _.groupBy(request.documents, 'type');
 
     $scope.upload = function(file, documentFile) {
       UploadService.upload(request, $scope.filesVM, file, documentFile);
