8c30214dbd7a2df58d29e6a790740a16e45ca640
Florian Delezenne <del.florian@gmail.com>
2015-12-16 17:58:01 +0100

    Fix request history

--- client/components/documents/documents.filter.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('impactApp')
-  .filter('documentFilter', function(documentTypes) {
-    return function(input) {
-      var document = _.find(documentTypes, {id: input});
-      return document.label;
-    };
-  });
