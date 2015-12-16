e4d03dc6806370f13f9dffa0b36093bd43774f80
Florian Delezenne <del.florian@gmail.com>
2015-12-16 17:08:02 +0100

    Fix document types

--- client/components/upload/upload.service.js
+++ client/components/upload/upload.service.js
@@ -1,38 +1,39 @@
 'use strict';
 
 angular.module('impactApp')
-  .factory('UploadService', function UploadService(Upload, documentTypes) {
-    var documentTypesById = _.indexBy(documentTypes, 'id');
-
+  .factory('UploadService', function UploadService(Upload, DocumentResource) {
     return {
       upload: function(request, filesVM, file, document) {
-        if (!filesVM[document]) {
-          filesVM[document] = [];
-        }
-
-        var type = filesVM[document];
-        var length = type.length;
-
-        Upload.upload({
-          url: 'api/requests/' + request.shortId + '/document',
-          method: 'POST',
-          file: file,
-          data: {
-              type: document,
-              category: documentTypesById[document].category
-            }
-        })
-        .progress(function(evt) {
-          if (evt.config.file) {
-            type[length] = {
-              name: evt.config.file.name,
-              progress: parseInt(100.0 * evt.loaded / evt.total)
-            };
+        DocumentResource.query().$promise.then(function(result) {
+          var documentTypesById =  _.indexBy(result, 'id');
+          if (!filesVM[document]) {
+            filesVM[document] = [];
           }
-        })
-        .success(function(data) {
-          request.documents.push(data);
-          type[length] = data;
+
+          var type = filesVM[document];
+          var length = type.length;
+
+          Upload.upload({
+            url: 'api/requests/' + request.shortId + '/document',
+            method: 'POST',
+            file: file,
+            data: {
+                type: document,
+                category: documentTypesById[document].category
+              }
+          })
+          .progress(function(evt) {
+            if (evt.config.file) {
+              type[length] = {
+                name: evt.config.file.name,
+                progress: parseInt(100.0 * evt.loaded / evt.total)
+              };
+            }
+          })
+          .success(function(data) {
+            request.documents.push(data);
+            type[length] = data;
+          });
         });
       }
     };
