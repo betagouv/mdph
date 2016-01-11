80096a208dfd3f34ba360786f9cdefdea878cc89
Florian Delezenne <del.florian@gmail.com>
2016-01-11 17:32:26 +0100

    Working with placeholders to prototype

--- client/app/espace_perso/mes_profils/profil/profil.controller.js
+++ client/app/espace_perso/mes_profils/profil/profil.controller.js
@@ -2,4 +2,59 @@
 
 angular.module('impactApp').controller('ProfilCtrl', function($scope, ProfileResource, currentUser, profileId) {
   $scope.profile = ProfileResource.get({userId: currentUser._id, id: profileId});
+
+  $scope.options = {
+    identites: {
+      beneficiaire: {
+        title: 'Bénéficiaire',
+        subhead: 'Section incomplète',
+        icon: 'fa-user'
+      },
+
+      autorite: {
+        title: 'Autorité parentale',
+        content: 'Autorité parentale ou délégation d\'autorité, obligatoire si le bénéficiaire est mineur.',
+        subhead: 'Section incomplète',
+        icon: 'fa-users'
+      }
+    },
+
+    projet: {
+      vieQuotidienne: {
+        title: 'Vie quotidienne',
+        content: '',
+        subhead: 'Section incomplète',
+        icon: 'fa-home'
+      },
+
+      vieScolaire: {
+        title: 'Vie scolaire',
+        content: '',
+        subhead: 'Section incomplète',
+        icon: 'fa-university'
+      },
+
+      vieTravail: {
+        title: 'Vie au travail',
+        content: '',
+        subhead: 'Section incomplète',
+        icon: 'fa-industry'
+      }
+    },
+
+    autre: {
+      aidant: {
+        title: 'Vie de votre aidant familial',
+        subhead: 'Section incomplète',
+        content: 'Si votre aidant familial (la personne qui s\'occupe de vous au quotidien) souhaite exprimer sa situation et ses besoins.',
+        icon: 'fa-male'
+      },
+      particulieres: {
+        title: 'Situations particulières',
+        subhead: 'Section incomplète',
+        content: 'Si vous vous trouvez dans une situation nécessitant une attention particulière.',
+        icon: 'fa-warning'
+      }
+    }
+  };
 });
