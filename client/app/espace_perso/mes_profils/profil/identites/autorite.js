c2ccf6445b15e3572fc86208581af57e372f345b
Florian Delezenne <del.florian@gmail.com>
2016-01-15 14:55:40 +0100

    Add request authority

--- client/app/espace_perso/mes_profils/profil/identites/autorite.js
+++ client/app/espace_perso/mes_profils/profil/identites/autorite.js
@@ -3,20 +3,23 @@
 angular.module('impactApp')
   .config(function($stateProvider) {
     $stateProvider
-      .state('espace_perso.mes_profils.profil.beneficiaire', {
-        url: '/identite-beneficiaire',
+      .state('espace_perso.mes_profils.profil.autorite', {
+        url: '/identite-autorite',
         data: {
-          title: 'Identité du bénéficiaire'
+          title: 'Identité de l\'autorité parentale'
         },
-        templateUrl: 'app/espace_perso/mes_profils/profil/identites/beneficiaire.html',
+        templateUrl: 'app/espace_perso/mes_profils/profil/identites/autorite.html',
         resolve: {
           identite: function(profile) {
             if (!profile.identites) {
               profile.identites = {};
-              profile.identites.beneficiaire = {};
             }
 
-            return profile.identites.beneficiaire;
+            if (!profile.identites.autorite) {
+              profile.identites.autorite = {};
+            }
+
+            return profile.identites.autorite;
           }
         },
         controller: function($scope, $state, profile, currentUser, identite, typesVoies) {
@@ -24,14 +27,14 @@ angular.module('impactApp')
           $scope.typesVoies = typesVoies;
 
           $scope.disableAddress = function() {
-            return ($scope.identite.adresseInconnue ? true : false);
+            return ($scope.identite && $scope.identite.adresseInconnue ? true : false);
           };
 
           $scope.submit = function(form) {
             if (form.$invalid) {
               form.showError = true;
             } else {
-              profile.identites.beneficiaire = identite;
+              profile.identites.autorite = identite;
               profile.$save({userId: currentUser._id}, function() {
                 $state.go('^');
               });
