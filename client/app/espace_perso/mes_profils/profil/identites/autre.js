a2f243ce2f8b5aa30599e98e293144ef0aa4f99a
Florian Delezenne <del.florian@gmail.com>
2016-01-18 11:52:20 +0100

    Ajout beneficiaire

---
+++ client/app/espace_perso/mes_profils/profil/identites/autre.js
@@ -0,0 +1,41 @@
+'use strict';
+
+angular.module('impactApp')
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('espace_perso.mes_profils.profil.autre', {
+        url: '/identite-autre',
+        data: {
+          title: 'Identité de la personne vous aidant dans cette démarche'
+        },
+        templateUrl: 'app/espace_perso/mes_profils/profil/identites/autre.html',
+        resolve: {
+          identite: function(profile) {
+            if (!profile.identites) {
+              profile.identites = {};
+            }
+
+            if (!profile.identites.autre) {
+              profile.identites.autre = {};
+            }
+
+            return profile.identites.autre;
+          }
+        },
+        controller: function($scope, $state, profile, currentUser, identite, typesVoies) {
+          $scope.identite = identite;
+          $scope.typesVoies = typesVoies;
+
+          $scope.submit = function(form) {
+            if (form.$invalid) {
+              form.showError = true;
+            } else {
+              profile.identites.autre = identite;
+              profile.$save({userId: currentUser._id}, function() {
+                $state.go('^');
+              });
+            }
+          };
+        }
+      });
+  });
