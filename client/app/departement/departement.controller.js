b0377ee3a5711c160420b1c6863051620101f59d
rquelen <rquelen@octo.com>
2016-02-16 19:29:38 +0100

    add map for mdph contact

--- client/app/departement/departement.controller.js
+++ client/app/departement/departement.controller.js
@@ -1,9 +1,11 @@
 'use strict';
 
 angular.module('impactApp')
-  .controller('DepartementCtrl', function($scope, currentUser, currentMdph) {
+  .controller('DepartementCtrl', function($scope, $rootScope, $state, $timeout, currentUser, currentMdph) {
     $scope.$emit('event:mdph-changed', currentMdph);
     $scope.currentUser = currentUser;
+
+    L.mapbox.accessToken = 'pk.eyJ1IjoicnF1ZWxlbiIsImEiOiJjaWtwaG15YjYwMGJ1dzVtNnE3dTZxc3VsIn0.QnrYAVJmcu9JS523vwLR6A';
 
     $scope.isAdmin = function() {
       if (!currentUser) {
@@ -19,4 +21,46 @@ angular.module('impactApp')
           return false;
       }
     };
+
+    $timeout(function() {
+      var mapBoxOptions = {paddingTopLeft: [200, 0]};
+      var map = L.mapbox.map('map', 'mapbox.streets').setView([currentMdph.coordinates.coordy, currentMdph.coordinates.coordx], 14);
+
+      // Disable drag and zoom handlers.
+      map.dragging.disable();
+      map.touchZoom.disable();
+      map.doubleClickZoom.disable();
+      map.scrollWheelZoom.disable();
+      map.keyboard.disable();
+
+      // Disable tap handler, if present.
+      if (map.tap) {
+        map.tap.disable();
+      }
+
+      L.mapbox.featureLayer({
+        // this feature is in the GeoJSON format: see geojson.org
+        // for the full specification
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+
+          // coordinates here are in longitude, latitude order because
+          // x, y is the standard for GeoJSON and many formats
+          coordinates: [
+            currentMdph.coordinates.coordx,
+            currentMdph.coordinates.coordy
+          ]
+        },
+        properties: {
+          title: currentMdph.name,
+          description: currentMdph.address,
+          'marker-size': 'large',
+          'marker-color': '#333E54',
+          'marker-symbol': 'building'
+        }
+      }).addTo(map);
+
+    });
+
   });
--- server/api/mdph/mdph.model.js
+++ server/api/mdph/mdph.model.js
@@ -7,7 +7,14 @@ var MdphSchema = new Schema({
   zipcode:  { type: String, unique: true },
   name:     { type: String },
   logo:     { type: String },
-  enabled:  { type: Boolean }
+  enabled:  { type: Boolean },
+  coordinates: {
+    coordx:   { type: String },
+    coordy:   { type: String },
+  },
+  address:  { type: String },
+  phone:    { type: String },
+  email:    { type: String }
 });
 
 module.exports = mongoose.model('Mdph', MdphSchema);
