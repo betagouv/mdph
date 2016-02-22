ef9c33c8415737d8696200c7089dc18a1856c3f2
rquelen <rquelen@octo.com>
2016-02-22 13:03:21 +0100

    test with authentication

--- server/api/request/request.spec.js
+++ server/api/request/request.spec.js
@@ -5,12 +5,15 @@ var Request = require('./request.model');
 var controller = require('./request.controller');
 
 var Actions = require('../../components/actions').actions;
-
 var serverTest = require('../../test/utils/server');
+
+var User = require('../user/user.model');
 
 describe('Request Model', function() {
   var server = serverTest();
   var api = server.api;
+  var auth = server.auth;
+  var getToken = server.token;
 
   before(function(done) {
     // Clear mdphs before testing
@@ -43,21 +46,35 @@ describe('Request Model', function() {
   describe('When the user is authenticated', function() {
 
     it('should return 200', function(done) {
-      api()
-        .put('/api/requests/1234')
-        .expect(200, done);
+      //given
+      var newRequest = new Request({shortId: '1234'});
+
+      //when
+      newRequest.save(function() {
+        var token = getToken();
+
+        //then
+        api()
+          .put('/api/requests/1234?access_token=' + token)
+          .expect(200, done);
+      });
     });
 
   });
 
   describe('When the user is not authenticated', function() {
+    var newRequest = new Request({shortId: '1234'});
 
-    it('should return 401', function(done) {
-      api()
-        .put('/api/requests/1234')
-        .expect(401, done);
+    //when
+    newRequest.save(function() {
+
+      //then
+      it('should return 401', function(done) {
+        api()
+          .put('/api/requests/1234')
+          .expect(401, done);
+      });
     });
-
   });
 
 });
--- server/test/utils/server.js
+++ server/test/utils/server.js
@@ -2,29 +2,46 @@ var app = require('../../app');
 var supertest = require('supertest');
 var User = require('../../api/user/user.model');
 
+var signToken = require('../../auth/auth.service').signToken;
+var jwt = require('jsonwebtoken');
+var config = require('../../config/environment');
+
 module.exports = function() {
   var port = 3001;
   var server;
-
-  var user = new User({
-    provider: 'local',
-    name: 'Fake User',
-    email: 'test@test.com',
-    password: 'password'
-  });
+  var token;
 
   var apiSuperTest = function() {
     return supertest.agent('http://localhost:' + port);
   };
 
+  var apiSuperTestAuth = function() {
+    return apiSuperTest().set('authorization', 'Bearer ' + token);
+  };
+
   beforeEach(function(done) {
+    var user = new User({
+      provider: 'local',
+      name: 'Fake User',
+      email: 'test@test.com',
+      password: 'password',
+      role: 'adminMdph'
+    });
+
     server = app.listen(port);
     user.save(function(err) {
       if (err) {
         return done(err);
       }
 
-      apiSuperTest().post('/auth/local');
+      token = jwt.sign({_id: user._id }, config.secrets.session, { expiresIn: 60 * 60 * 5 });
+      done();
+    });
+  });
+
+  afterEach(function(done) {
+    User.remove(function() {
+      done();
     });
   });
 
@@ -32,13 +49,12 @@ module.exports = function() {
     server.close(done);
   });
 
-  afterEach(function(done) {
-    user.remove(done);
-  });
-
   return {
     api: apiSuperTest,
-    api: apiSuperTest
+    auth: apiSuperTestAuth,
+    token: function() {
+      return token;
+    }
   };
 
 };
