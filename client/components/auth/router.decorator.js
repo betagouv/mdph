2ce11b103c2745c000ac4d5118d417f5381338e5
Florian Delezenne <del.florian@gmail.com>
2016-03-09 17:22:00 +0100

    Re-plug redirectTo

--- client/components/auth/router.decorator.js
+++ client/components/auth/router.decorator.js
@@ -5,8 +5,21 @@
   angular.module('impactApp.auth')
     .run(function($rootScope, $state, Auth) {
       // Redirect to login if route requires auth and the user is not logged in, or doesn't have required role
-      $rootScope.$on('$stateChangeStart', function(event, next) {
+      $rootScope.$on('$stateChangeStart', function(event, next, nextParams) {
         if (!next.authenticate) {
+          return;
+        }
+
+        if (typeof next.redirectTo !== 'undefined') {
+          event.preventDefault();
+          if (typeof next.redirectTo === 'string') {
+
+            $state.go(next.redirectTo, nextParams);
+          } else {
+            var params = _.assign(nextParams, next.redirectTo.params);
+            $state.go(next.redirectTo.url, params);
+          }
+
           return;
         }
 
