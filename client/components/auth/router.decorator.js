c77a63483ca7e3e626b9a9c6aa57b6504b01e80f
Florian Delezenne <del.florian@gmail.com>
2016-03-10 13:57:40 +0100

    Resolve redirectTo even if state doesn't need auth

--- client/components/auth/router.decorator.js
+++ client/components/auth/router.decorator.js
@@ -6,14 +6,22 @@
     .run(function($rootScope, $state, Auth) {
       // Redirect to login if route requires auth and the user is not logged in, or doesn't have required role
       $rootScope.$on('$stateChangeStart', function(event, next, nextParams) {
-        if (!next.authenticate) {
+        if (typeof next.authenticate !== 'undefined') {
+          Auth.isLoggedIn(_.noop).then(is => {
+            if (is) {
+              return;
+            }
+
+            event.preventDefault();
+            $state.go('login');
+          });
+
           return;
         }
 
         if (typeof next.redirectTo !== 'undefined') {
           event.preventDefault();
           if (typeof next.redirectTo === 'string') {
-
             $state.go(next.redirectTo, nextParams);
           } else {
             var params = _.assign(nextParams, next.redirectTo.params);
@@ -21,28 +29,6 @@
           }
 
           return;
-        }
-
-        if (typeof next.authenticate === 'string') {
-          Auth.hasRole(next.authenticate, _.noop).then(has => {
-            if (has) {
-              return;
-            }
-
-            event.preventDefault();
-            return Auth.isLoggedIn(_.noop).then(is => {
-              $state.go(is ? 'departement' : 'login');
-            });
-          });
-        } else {
-          Auth.isLoggedIn(_.noop).then(is => {
-            if (is) {
-              return;
-            }
-
-            event.preventDefault();
-            $state.go('departement');
-          });
         }
       });
     });
