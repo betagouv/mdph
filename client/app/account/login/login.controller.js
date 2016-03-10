31f98f71195cea8e85d77045783877505bfa401c
Florian Delezenne <del.florian@gmail.com>
2016-03-10 19:11:10 +0100

    Redirect after login, hide ugly messages

--- client/app/account/login/login.controller.js
+++ client/app/account/login/login.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('impactApp')
-  .controller('LoginCtrl', function($rootScope, $scope, Auth, $location, $state) {
+  .controller('LoginCtrl', function($rootScope, $scope, Auth, $location, $state, currentMdph) {
     $scope.user = {};
     $scope.error = null;
 
@@ -11,19 +11,23 @@ angular.module('impactApp')
           email: form.email.$modelValue,
           password: form.password.$modelValue
         })
-        .then(function(user) {
-          if ($rootScope.returnToState) {
-            return $state.go($rootScope.returnToState.name, $rootScope.returnToStateParams, {reload: true});
-          }
-
-          if (user.role === 'adminMdph') {
-            return $state.go('dashboard.workflow', {codeDepartement: user.mdph  && user.mdph.zipcode}, {reload: true});
-          }
-
-          return $state.go('espace_perso.mes_profils', {}, {reload: true});
-        })
         .catch(function(err) {
           $scope.error = err.message;
+        })
+        .then(function(user) {
+          if ($rootScope.returnToState) {
+            return $state.go($rootScope.returnToState.name, $rootScope.returnToStateParams);
+          }
+
+          if (Auth.isAdmin(user)) {
+            return $state.go('dashboard.workflow', {codeDepartement: currentMdph.zipcode});
+          }
+
+          if (Auth.isAdminMdph(user)) {
+            return $state.go('dashboard.workflow', {codeDepartement: user.mdph  && user.mdph.zipcode});
+          }
+
+          return $state.go('espace_perso.mes_profils');
         });
       }
     };
--- client/components/auth/router.decorator.js
+++ client/components/auth/router.decorator.js
@@ -1,11 +1,22 @@
 'use strict';
 
 (function() {
-
   angular.module('impactApp.auth')
     .run(function($rootScope, $state, Auth) {
       // Redirect to login if route requires auth and the user is not logged in, or doesn't have required role
       $rootScope.$on('$stateChangeStart', function(event, next, nextParams) {
+        if (typeof next.redirectTo !== 'undefined') {
+          event.preventDefault();
+          if (typeof next.redirectTo === 'string') {
+            $state.go(next.redirectTo, nextParams);
+          } else {
+            var params = _.assign(nextParams, next.redirectTo.params);
+            $state.go(next.redirectTo.url, params);
+          }
+
+          return;
+        }
+
         if (typeof next.authenticate !== 'undefined') {
           Auth.isLoggedIn(_.noop).then(is => {
             if (is) {
@@ -15,18 +26,6 @@
             event.preventDefault();
             $state.go('login');
           });
-
-          return;
-        }
-
-        if (typeof next.redirectTo !== 'undefined') {
-          event.preventDefault();
-          if (typeof next.redirectTo === 'string') {
-            $state.go(next.redirectTo, nextParams);
-          } else {
-            var params = _.assign(nextParams, next.redirectTo.params);
-            $state.go(next.redirectTo.url, params);
-          }
 
           return;
         }
