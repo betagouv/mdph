fc63fc7adf93464721f113c1182eb1d78d578a21
Florian Delezenne <del.florian@gmail.com>
2016-03-11 11:33:08 +0100

    Only redirect if needed

--- client/components/auth/router.decorator.js
+++ client/components/auth/router.decorator.js
@@ -5,21 +5,20 @@
     .run(function($rootScope, $state, Auth) {
       // Redirect to login if route requires auth and the user is not logged in, or doesn't have required role
       $rootScope.$on('$stateChangeStart', function(event, next, nextParams) {
-        if (typeof next.redirectTo !== 'undefined') {
-          event.preventDefault();
-          if (typeof next.redirectTo === 'string') {
-            $state.go(next.redirectTo, nextParams);
-          } else {
-            var params = _.assign(nextParams, next.redirectTo.params);
-            $state.go(next.redirectTo.url, params);
-          }
-
-          return;
-        }
-
         if (typeof next.authenticate !== 'undefined') {
           Auth.isLoggedIn(_.noop).then(is => {
             if (is) {
+              if (typeof next.redirectTo !== 'undefined') {
+                event.preventDefault();
+
+                if (typeof next.redirectTo === 'string') {
+                  $state.go(next.redirectTo, nextParams);
+                } else {
+                  let toParams = _.assign(nextParams, next.redirectTo.params);
+                  $state.go(next.redirectTo.url, toParams);
+                }
+              }
+
               return;
             }
 
@@ -29,6 +28,19 @@
 
           return;
         }
+
+        if (typeof next.redirectTo !== 'undefined') {
+          event.preventDefault();
+
+          if (typeof next.redirectTo === 'string') {
+            $state.go(next.redirectTo, nextParams);
+          } else {
+            let toParams = _.assign(nextParams, next.redirectTo.params);
+            $state.go(next.redirectTo.url, toParams);
+          }
+        }
+
+        return;
       });
     });
 })();
