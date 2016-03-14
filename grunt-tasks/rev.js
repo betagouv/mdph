f71f8e35cb0e69c55c361c0078a16eade82c4c35
Florian Delezenne <del.florian@gmail.com>
2016-03-14 11:11:27 +0100

    Do not rev assets

--- grunt-tasks/rev.js
+++ grunt-tasks/rev.js
@@ -3,7 +3,6 @@ module.exports = {
     files: {
       src: [
         '<%= app.dirs.dist %>/<%= app.dirs.client %>/!(bower_components){,*/}*.{js,css}',
-        '<%= app.dirs.dist %>/<%= app.dirs.client %>/assets/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
         '<%= app.dirs.dist %>/<%= app.dirs.client %>/assets/fonts/*'
       ]
     }
