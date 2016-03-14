68e68162bc13984f745ad5fc66a4000a117233e9
Florian Delezenne <del.florian@gmail.com>
2016-03-14 16:41:41 +0100

    Repair download bardcode

--- server/api/document-category/index.js
+++ server/api/document-category/index.js
@@ -1,6 +1,5 @@
 import {Router} from 'express';
 import * as controller from './document-category.controller';
-import {isAuthenticated} from '../../auth/auth.service';
 import multer from 'multer';
 
 var storage = multer.memoryStorage();
@@ -15,13 +14,14 @@ router.get('/unclassifiedCategory', controller.getUnclassifiedCategory);
 router.post('/document-types', controller.updateDocumentType);
 router.get('/document-types', controller.showUncategorizedDocumentTypes);
 
+router.post('/:categoryId/file', upload.single('file'), controller.saveDocumentCategoryFile);
+router.get('/:categoryId/file', controller.getDocumentCategoryFile);
+
 //router.put('/', isAuthenticated(), controller.updateDocumentCategories);
 
 // router.put('/:categoryId', isAuthenticated(), controller.updateDocumentCategory);
 // router.delete('/:categoryId', isAuthenticated(), controller.removeDocumentCategory);
 //
-// router.post('/:categoryId/file', isAuthenticated(), upload.single('file'), controller.saveDocumentCategoryFile);
-// router.get('/:categoryId/file', isAuthenticated(), controller.getDocumentCategoryFile);
 
 // router.post('/:categoryId/moveDocument/:newCategoryId', isAuthenticated(), controller.updateDocument);
 
