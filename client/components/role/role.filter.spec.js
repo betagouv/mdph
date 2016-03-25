b893385268a062dacbcbc22381ea890b1cf2c30b
rquelen <rquelen@octo.com>
2016-03-25 18:01:32 +0100

    I <3 tests

--- client/components/role/role.filter.spec.js
+++ client/components/role/role.filter.spec.js
@@ -12,15 +12,56 @@ describe('Filter: role', function() {
     });
   });
 
-  it('should return the prettystr version of the adminMdph role', function() {
-    //given
-    var text = 'adminMdph';
+  describe('admin', function() {
+    it('should return Administrateur', function() {
+      //given
+      var text = 'admin';
 
-    //when
-    var result = $filter('role')(text);
+      //when
+      var result = $filter('role')(text);
 
-    //then
-    expect(result).toBe('Gestionnaire MDPH');
+      //then
+      expect(result).toBe('Administrateur');
+    });
+  });
+
+  describe('adminMdph', function() {
+    it('should return Gestionnaire MDPH', function() {
+      //given
+      var text = 'adminMdph';
+
+      //when
+      var result = $filter('role')(text);
+
+      //then
+      expect(result).toBe('Gestionnaire MDPH');
+    });
+  });
+
+  describe('user', function() {
+    it('should return Utilisateur', function() {
+      //given
+      var text = 'user';
+
+      //when
+      var result = $filter('role')(text);
+
+      //then
+      expect(result).toBe('Utilisateur');
+    });
+  });
+
+  describe('default', function() {
+    it('should return an empty string', function() {
+      //given
+      var text = 'FakeRole';
+
+      //when
+      var result = $filter('role')(text);
+
+      //then
+      expect(result).toBe('');
+    });
   });
 
 });
--- client/components/string/capitalize.filter.spec.js
+++ client/components/string/capitalize.filter.spec.js
@@ -10,37 +10,41 @@ describe('Capitalize.filter', function() {
     });
   });
 
-  it('should capitalize first letter of a word, after a \' but not after an accent', function() {
-    //given
-    var text = 'type d\'hébergement';
+  describe('capitalize', function() {
+    it('should capitalize first letter of a word, after a \' but not after an accent', function() {
+      //given
+      var text = 'type d\'hébergement';
 
-    //when
-    var result = this.$filter('capitalize')(text);
+      //when
+      var result = this.$filter('capitalize')(text);
 
-    //then
-    expect(result).toBe('Type D\'Hébergement');
+      //then
+      expect(result).toBe('Type D\'Hébergement');
+    });
+
+    it('should capitalize after a \'-\'', function() {
+      //given
+      var text = 'courmont-nazaraly, naël';
+
+      //when
+      var result = this.$filter('capitalize')(text);
+
+      //then
+      expect(result).toBe('Courmont-Nazaraly, Naël');
+    });
   });
 
-  it('should capitalize after a \'-\'', function() {
-    //given
-    var text = 'courmont-nazaraly, naël';
+  describe('capitalizeString', function() {
+    it('should only capitalize the first letter of the string', function() {
+      //given
+      var text = 'type d\'hébergement';
 
-    //when
-    var result = this.$filter('capitalize')(text);
+      //when
+      var result = this.$filter('capitalizeString')(text);
 
-    //then
-    expect(result).toBe('Courmont-Nazaraly, Naël');
-  });
-
-  it('should only capitalize the first letter of the string', function() {
-    //given
-    var text = 'type d\'hébergement';
-
-    //when
-    var result = this.$filter('capitalizeString')(text);
-
-    //then
-    expect(result).toBe('Type d\'hébergement');
+      //then
+      expect(result).toBe('Type d\'hébergement');
+    });
   });
 
 });
