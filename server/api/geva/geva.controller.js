7f1c264af6cb25b48c51dbdbd13affa5b5434c9e
Florian Delezenne <del.florian@gmail.com>
2016-04-05 17:59:52 +0200

    Maj EXCEL

--- server/api/geva/geva.controller.js
+++ server/api/geva/geva.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import _ from 'lodash';
-import questions from './questions.json';
+import questions from './questions_v9.json';
 
 export function index(req, res) {
   return res.json(questions);
