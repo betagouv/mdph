6179c3a2f86a69afdad8715d685886962e0e70ad
Florian Delezenne <del.florian@gmail.com>
2016-04-15 16:09:20 +0200

    Bigger timeout, redirect to mdph signin

--- client/components/auth/interceptor.service.js
+++ client/components/auth/interceptor.service.js
@@ -1,7 +1,7 @@
 'use strict';
 
 (function() {
-  function authInterceptor($rootScope, $q, $cookies, $injector, Util) {
+  function authInterceptor($rootScope, $q, $cookies, $injector, $window, Util) {
     var state;
 
     return {
@@ -18,7 +18,13 @@
       // Intercept 401s and redirect you to login
       responseError(response) {
         if (response.status === 401) {
-          (state || (state = $injector.get('$state'))).go('login', {codeDepartement: '14'});
+          var state = (state || (state = $injector.get('$state')));
+          var path = $window.location.pathname.split('/');
+          if (path[0] === 'mdph') {
+            state.go('login', {codeDepartement: path[1]});
+          } else {
+            state.go('main');
+          }
 
           // remove any stale tokens
           $cookies.remove('token');
