1033fddfce71d8362c08d8675ab62a3f1c086dc3
Florian Delezenne <del.florian@gmail.com>
2016-04-22 10:48:22 +0200

    New geva v11

--- server/api/geva/geva.controller.js
+++ server/api/geva/geva.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import _ from 'lodash';
-import questions from './questions_v9.json';
+import questions from './questions.json';
 
 export function index(req, res) {
   return res.json(questions);
