fc267ebc4543e6b3a62703b5def49c31f7fa5432
Florian Delezenne <del.florian@gmail.com>
2017-01-20 12:27:27 +0100

    Put dependencies twice so that tests work

--- karma.conf.js
+++ karma.conf.js
@@ -31,6 +31,7 @@ module.exports = function(config) {
       'client/bower_components/mapbox.js/mapbox.js',
       'client/bower_components/moment/moment.js',
       'client/bower_components/ng-file-upload/ng-file-upload.js',
+      'client/bower_components/ng-file-upload-shim/ng-file-upload-shim.js',
       'client/bower_components/ngstorage/ngStorage.js',
       'client/bower_components/angular-mocks/angular-mocks.js',
       'client/bower_components/angular-scenario/angular-scenario.js',
