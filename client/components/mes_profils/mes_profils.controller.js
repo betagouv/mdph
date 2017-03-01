e5a909d473cf35ba386c9ba2afb84dc8698333d1
Florian Delezenne <del.florian@gmail.com>
2017-03-01 14:15:54 +0100

    Remove unused dep

--- client/components/mes_profils/mes_profils.controller.js
+++ client/components/mes_profils/mes_profils.controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('impactApp').controller('MesProfilsCtrl', function($state, $modal, ProfileResource, toastr) {
+angular.module('impactApp').controller('MesProfilsCtrl', function($state, $modal, ProfileResource) {
   this.profiles = ProfileResource.query({userId: this.user._id});
 
   this.addProfile = function() {
