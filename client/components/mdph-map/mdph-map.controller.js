df8a5c6c8be603363d5ed5ab63d3f0d26d8954e3
Florian Delezenne <del.florian@gmail.com>
2017-03-30 17:01:13 +0200

    ETQ MDPH, je souhaites renseigner plusieurs adresses sur la carte

--- client/components/mdph-map/mdph-map.controller.js
+++ client/components/mdph-map/mdph-map.controller.js
@@ -2,48 +2,97 @@
 
 angular.module('impactApp')
   .controller('MdphMapCtrl', function() {
+    this.locations = this.mdph && this.mdph.locations;
+
     L.mapbox.accessToken = 'pk.eyJ1IjoiaW1wYWN0LW1hcGJveCIsImEiOiJjaWt6bmpqYTUwMDcwd29tNDRpczM2N2pwIn0.Qh9eYg3TMD00z22WzmDXyQ';
 
     (() => {
-      if (!this.mdph || !this.mdph.coordinates) {
+      if (!this.locations || this.locations.length === 0) {
         return;
       }
 
-      var map = L.mapbox.map('map', 'mapbox.streets').setView([this.mdph.coordinates.coordy, this.mdph.coordinates.coordx], 14);
+      const map = L.mapbox.map('map', 'mapbox.streets');
+
+      if (this.locations.length === 1) {
+        this.location = this.locations[0];
+        map.setView([this.location.coordinates.coordy, this.location.coordinates.coordx], 14);
+      } else {
+        const bounds = this.locations.map(location => {
+          return [location.coordinates.coordy, location.coordinates.coordx];
+        });
+        map.fitBounds(bounds);
+      }
+
 
       // Disable drag and zoom handlers.
-      map.dragging.disable();
-      map.touchZoom.disable();
-      map.doubleClickZoom.disable();
       map.scrollWheelZoom.disable();
-      map.keyboard.disable();
 
       // Disable tap handler, if present.
       if (map.tap) {
         map.tap.disable();
       }
 
-      L.mapbox.featureLayer({
-        // this feature is in the GeoJSON format: see geojson.org
-        // for the full specification
-        type: 'Feature',
-        geometry: {
-          type: 'Point',
+      L.Map = L.Map.extend({
+          openPopup: function(popup) {
+              this._popup = popup;
 
-          // coordinates here are in longitude, latitude order because
-          // x, y is the standard for GeoJSON and many formats
-          coordinates: [
-            this.mdph.coordinates.coordx,
-            this.mdph.coordinates.coordy
-          ]
-        },
-        properties: {
-          title: this.mdph.name,
-          description: this.mdph.address,
-          'marker-size': 'large',
-          'marker-color': '#333E54',
-          'marker-symbol': 'building'
+              return this.addLayer(popup).fire('popupopen', {
+                  popup: this._popup
+              });
+          }
+      });
+
+      let first = true;
+      this.locations.forEach(location => {
+        const featureLayer = {
+          // this feature is in the GeoJSON format: see geojson.org
+          // for the full specification
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+
+            // coordinates here are in longitude, latitude order because
+            // x, y is the standard for GeoJSON and many formats
+            coordinates: [
+              location.coordinates.coordx,
+              location.coordinates.coordy
+            ]
+          },
+          properties: {
+            title: location.name,
+            description: `
+              <table>
+                <tbody>
+                  <tr>
+                    <td class="cell-icon"><i class="fa fa-home"></i></td>
+                    <td><span>${location.address}</span></td>
+                  </tr>
+                  <tr>
+                    <td class="cell-icon"><i class="fa fa-phone"></i></td>
+                    <td><span>${location.phone}</span></td>
+                  </tr>
+                  <tr>
+                    <td class="cell-icon"><i class="fa fa-envelope"></i></td>
+                    <td><a href="mailto:${location.email}" title="Envoyer un mail de contact">${location.email}</a></td>
+                  </tr>
+                  <tr>
+                    <td class="cell-icon"><i class="fa fa-clock-o"></i></td>
+                    <td><span>${location.schedule}</span></td>
+                  </tr>
+                </tbody>
+              </table>
+            `,
+            'marker-size': 'large',
+            'marker-color': '#333E54',
+            'marker-symbol': 'building'
+          }
+        };
+
+        const popup = L.mapbox.featureLayer(featureLayer).addTo(map);
+        if (first) {
+          popup.openPopup();
+          first = false;
         }
-      }).addTo(map);
+      });
     })();
   });
--- server/api/mdph/mdph.model.js
+++ server/api/mdph/mdph.model.js
@@ -3,19 +3,29 @@
 import mongoose, {Schema} from 'mongoose';
 
 var MdphSchema = new Schema({
-  zipcode:  { type: String, unique: true },
-  name:     { type: String },
-  logo:     { type: String },
-  enabled:  { type: Boolean },
-  opened:   { type: Boolean },
-  coordinates: {
-    coordx:   { type: String },
-    coordy:   { type: String },
+  zipcode:      { type: String, unique: true },
+  name:         { type: String },
+  logo:         { type: String },
+  enabled:      { type: Boolean },
+  opened:       { type: Boolean },
+  likes:        [{ type: String }],
+  outsideLink: {
+    href:       { type: String },
+    label:      { type: String }
   },
-  address:  { type: String },
-  phone:    { type: String },
-  email:    { type: String },
-  likes:    [{ type: String }]
+  locations: [
+    {
+      photo:        { type: String },
+      address:      { type: String },
+      coordinates: {
+        coordx:     { type: String },
+        coordy:     { type: String },
+      },
+      phone:        { type: String },
+      email:        { type: String },
+      schedule:     { type: String },
+    }
+  ],
 });
 
 export default mongoose.model('Mdph', MdphSchema);
