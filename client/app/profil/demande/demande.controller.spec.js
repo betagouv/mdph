a8b96c8e1b1c1ae33622e81e4b5be8362ab52750
Fabien Champigny <fabien@champigny.name>
2017-04-05 16:36:06 +0200

    add tests

--- client/app/profil/demande/demande.controller.spec.js
+++ client/app/profil/demande/demande.controller.spec.js
@@ -28,27 +28,30 @@ describe('Controller: demande', function() {
 
   let currentUser = {};
 
-  let prestations = [
-    {
-      id: 'cartestationnement',
-      type: 'carte',
-      label: 'Carte stationnement',
-      title: 'Carte de stationnement',
-      description: 'La carte européenne de stationnement permet à son titulaire ou à la personne qui l\'accompagne de stationner sur les places réservées aux personnes handicapées.',
-      link: 'http://vosdroits.service-public.fr/particuliers/F2891.xhtml'
-    },
-    {
-      id: 'carteinvalidite',
-      type: 'carte',
-      label: 'Carte d\'invalidité ou de priorité',
-      title: 'Carte d\'invalidité ou de priorité',
-      description: 'La carte d\'invalidité civile a pour but d\'attester que son détenteur est handicapé et permet de bénéficier de certains droits spécifiques, notamment dans les transports. La carte de priorité permet d\'obtenir un droit de priorité pour l\'accès aux bureaux et guichets des administrations et services publics et aux transports publics.',
-      link: 'http://vosdroits.service-public.fr/particuliers/F2446.xhtml'
-    },
-  ];
-
   // load the service's module
   beforeEach(module('impactApp'));
+
+  it('should have 16 prestations', function() {
+    //given
+    var $scope = {};
+
+    //when
+    inject(function($controller) {
+      $controller('DemandeCtrl', {
+        $state: {
+          go() {}
+        },
+
+        $scope,
+        currentUser,
+        request,
+        RequestService,
+        toastr
+      });
+    });
+
+    expect($scope.prestations.length, 16);
+  });
 
   it('should select the right prestations', function() {
     //given
@@ -63,16 +66,15 @@ describe('Controller: demande', function() {
 
         $scope,
         currentUser,
-        prestations,
         request,
         RequestService,
         toastr
       });
     });
 
-    prestations[0].choice = true;
-    prestations[1].choice = true;
-    prestations[1].renouvellement = true;
+    $scope.cartestationnement.choice = true;
+    $scope.carteinvalidite.choice = true;
+    $scope.carteinvalidite.renouvellement = true;
     $scope.submit({$valid: true});
 
     //then
