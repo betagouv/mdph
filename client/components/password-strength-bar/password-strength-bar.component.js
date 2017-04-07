862184b5fab52043de44687b5986770df38cebeb
Florian Delezenne <del.florian@gmail.com>
2017-04-07 15:57:07 +0200

    Add style, move logic to controller

--- client/components/password-strength-bar/password-strength-bar.component.js
+++ client/components/password-strength-bar/password-strength-bar.component.js
@@ -3,11 +3,41 @@
 angular.module('impactApp')
   .component('passwordStrengthBar', {
     bindings: {
-      password: '='
+      password: '<'
     },
     templateUrl: 'components/password-strength-bar/password-strength-bar.html',
     controllerAs: 'passwordStrengthBarCtrl',
     controller() {
-      this.passwordStrength = zxcvbn;
+      const labels = [
+        'Très faible',
+        'Faible',
+        'Moyen',
+        'Fort',
+        'Excellent'
+      ];
+
+      const classes = [
+        'progress-bar-danger',
+        'progress-bar-danger',
+        'progress-bar-warning',
+        'progress-bar-info',
+        'progress-bar-success'
+      ];
+
+      this.refreshScore = (password) => {
+        const score = password ? zxcvbn(password).score : 0;
+        this.percent = score * 25;
+        this.label = labels[score];
+        this.class = classes[score];
+      };
+
+      this.$onInit = () => {
+        this.refreshScore(this.password);
+      };
+
+      this.$onChanges = (changes) => {
+        this.refreshScore(changes.password.currentValue);
+      };
+
     }
   });
