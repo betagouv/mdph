96b9ce6dc430189797af5db5abaf062f25806110
Florian Delezenne <del.florian@gmail.com>
2017-04-13 11:11:53 +0200

    Extract footer to a separate component

---
+++ client/components/footer/footer.component.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('impactApp').component('mdphFooter', {
+  templateUrl: 'components/footer/footer.html',
+  controller: function() {},
+  controllerAs: 'footer',
+  bindings: {
+    mdph: '<'
+  }
+});
