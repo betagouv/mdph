3b01d3f31becfa7cef85c55024a88c23f4f69f3f
Fabien Champigny <fabien@champigny.name>
2017-04-27 15:25:40 +0200

    Add departments goejson load file

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,7 +1,8 @@
 'use strict';
 
 angular.module('impactApp')
-  .controller('MainCtrl', function($scope, mdphs) {
+  .controller('MainCtrl', function($scope, mdphs, depsgeo) {
     $scope.$emit('event:mdph-none');
     $scope.mdphs = mdphs;
+    $scope.depsgeo = depsgeo;
   });
