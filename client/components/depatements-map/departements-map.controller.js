ca11151e1c1dc4dbad3099374acb377a9d52c887
Fabien Champigny <fabien@champigny.name>
2017-05-03 12:28:47 +0200

    Add pin click makes go to mdph page

--- client/components/depatements-map/departements-map.controller.js
+++ client/components/depatements-map/departements-map.controller.js
@@ -4,9 +4,29 @@ angular.module('impactApp')
   .controller('DepsMapCtrl', function() {
     L.mapbox.accessToken = 'pk.eyJ1IjoiaW1wYWN0LW1hcGJveCIsImEiOiJjaWt6bmpqYTUwMDcwd29tNDRpczM2N2pwIn0.Qh9eYg3TMD00z22WzmDXyQ';
 
+    // Center map to France center.
     const map = L.mapbox.map('map', 'mapbox.streets').setView([46.52863469527167,2.43896484375], 6);
 
+    // Load departments borders map.
     L.geoJson(this.depsgeo, { style: L.mapbox.simplestyle.style }).addTo(map);
+
+    this.mdphs.forEach(mdph => {
+      const options = {
+        title: mdph.name,
+        alt: '/mdph/' + mdph.zipcode
+      };
+
+      mdph.locations.forEach(location => {
+        // Only pin headquarters.
+        if (location.headquarters) {
+          L.marker([location.coordinates.coordy, location.coordinates.coordx], options)
+            .on('click', function() {
+              window.location = '/mdph/' + mdph.zipcode;
+            })
+            .addTo(map);
+        }
+      });
+    });
 
     // Disable drag and zoom handlers.
     map.scrollWheelZoom.disable();
@@ -15,5 +35,4 @@ angular.module('impactApp')
     if (map.tap) {
       map.tap.disable();
     }
-
   });
