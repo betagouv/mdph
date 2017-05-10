ce77b0239de06dcb07256edb0351e6b81227caef
Fabien Champigny <fabien@champigny.name>
2017-05-10 14:53:33 +0200

    Delete unused function

--- client/components/departements-select/departements-select.controller.js
+++ client/components/departements-select/departements-select.controller.js
@@ -3,10 +3,5 @@
 angular.module('impactApp')
   .controller('DepsSelectpCtrl', function(MdphResource) {
     MdphResource.query(mdphs => this.mdphs = mdphs);
-
     this.mdph = 0;
-    this.goToMdph = () => {
-      console.log('change', this.mdph);
-      //window.location = '/mdph/' + this.mdph;
-    };
   });
