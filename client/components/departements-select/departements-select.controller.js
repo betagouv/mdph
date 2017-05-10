7308455bbed0cbc7fb5da32c5ee6263ad74528b0
Fabien Champigny <fabien@champigny.name>
2017-05-10 17:56:49 +0200

    Add go to mdph

--- client/components/departements-select/departements-select.controller.js
+++ client/components/departements-select/departements-select.controller.js
@@ -1,7 +1,8 @@
 'use strict';
 
 angular.module('impactApp')
-  .controller('DepsSelectpCtrl', function(MdphResource) {
+  .controller('DepsSelectpCtrl', function(MdphResource, $state) {
     MdphResource.query(mdphs => this.mdphs = mdphs);
     this.mdph = 0;
+    this.goToMdph = item => $state.go('departement', {codeDepartement: item.zipcode});
   });
