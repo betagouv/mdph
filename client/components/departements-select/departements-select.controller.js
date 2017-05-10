a7eaf8856850e6b7170c654a280d6b6bb3fd9d8b
Fabien Champigny <fabien@champigny.name>
2017-05-10 18:00:48 +0200

    Make zipCode passed directly

--- client/components/departements-select/departements-select.controller.js
+++ client/components/departements-select/departements-select.controller.js
@@ -3,6 +3,5 @@
 angular.module('impactApp')
   .controller('DepsSelectpCtrl', function(MdphResource, $state) {
     MdphResource.query(mdphs => this.mdphs = mdphs);
-    this.mdph = 0;
-    this.goToMdph = item => $state.go('departement', {codeDepartement: item.zipcode});
+    this.goToMdph = zipcode => $state.go('departement', {codeDepartement: zipcode});
   });
