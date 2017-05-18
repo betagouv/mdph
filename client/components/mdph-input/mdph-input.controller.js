74e664bbfba4217c96474c93e24809d8871d4942
Fabien Champigny <fabien@champigny.name>
2017-05-18 18:12:54 +0200

    Add workable departement input

---
+++ client/components/mdph-input/mdph-input.controller.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('impactApp')
+  .controller('MdphInputCtrl', function($state) {
+
+    this.zipcode = '';
+
+    this.isOk =  () => {
+      const zipcodeRegex = /^([0-8][0-9]|9[0-5]|2A|2a|2B|2b|97[1-4]|976)$/;
+      return zipcodeRegex.test(this.zipcode);
+    };
+
+    this.go = () => {
+      $state.go('departement', {codeDepartement: this.zipcode});
+    };
+
+  });
