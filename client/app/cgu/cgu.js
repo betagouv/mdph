f2705cf93d840d99749f0592359b92f16488ba74
lcommeau <lcommeau@norsys.fr>
2017-05-23 18:24:52 +0200

    Ajout CGU dans le footer

---
+++ client/app/cgu/cgu.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('impactApp')
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('cgu', {
+        url: '/cgu',
+        parent: 'layout',
+        authenticate: true,
+        data: {
+          title: 'CGU'
+        },
+        templateUrl: 'app/cgu/cgu.html',
+        controller: 'MonCompteCtrl',
+      });
+  });
+
+  angular.module('impactApp')
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('cgu', {
+        url: '/cgu',
+        templateUrl: 'app/cgu/cgu.html'
+      });
+  });
