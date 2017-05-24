41b1a9bc5a4c628bfd3c1e47f733ad286ca15461
lcommeau <lcommeau@norsys.fr>
2017-05-24 13:32:55 +0200

    correction duplication cgu.js

--- client/app/cgu/cgu.js
+++ client/app/cgu/cgu.js
@@ -5,21 +5,6 @@ angular.module('impactApp')
     $stateProvider
       .state('cgu', {
         url: '/cgu',
-        parent: 'layout',
-        authenticate: true,
-        data: {
-          title: 'CGU'
-        },
-        templateUrl: 'app/cgu/cgu.html',
-        controller: 'MonCompteCtrl',
-      });
-  });
-
-angular.module('impactApp')
-  .config(function($stateProvider) {
-    $stateProvider
-      .state('cgu', {
-        url: '/cgu',
         templateUrl: 'app/cgu/cgu.html'
       });
   });
