401125da76ed6131ae0a645106a58eb5c4a3b8d6
Fabien Champigny <fabien@champigny.name>
2017-06-28 18:25:26 +0200

    Make multi level of responses

--- scripts/load-geva-questions/index.js
+++ scripts/load-geva-questions/index.js
@@ -46,19 +46,40 @@ function transform(worksheet, callback) {
   var QUESTION = 'F';
   var TYPE = 'G';
 
-  var REPONSE_LIBELLE = 'J';
+  var REPONSE_LIBELLE = ['J','K','L','M','N','O'];
   var REPONSE_CODE_VALEUR = 'I';
-
-  var SUBREPONSE_LIBELLE = 'K';
 
   // Current section and reponse working on
   var section = {};
   var reponse = {};
 
-  // Process until current row is empty
-  for (var row = START_ROW ; worksheet['I' + row] !== undefined ; row++) {
+  const isValidRow = row => worksheet['I' + row] !== undefined;
 
-    if (worksheet[SECTION + row]) {
+  const isSection = row => worksheet[SECTION + row];
+
+  const makeSection = (row, id) => ({
+    id: id,
+    Section: worksheet[SECTION + row].v,
+    Libelle: worksheet[LIBELLE + row].v,
+    Trajectoire: worksheet[TRAJECTOIRE + row].v,
+    Question: worksheet[QUESTION + row].v,
+    Type: worksheet[TYPE + row].v,
+    Reponses: []
+  });
+
+  const isResponseLevel = (row, level) => worksheet[REPONSE_LIBELLE[level] + row];
+
+  const makeReponse = (worksheet, row, level, Reponses) => ({
+    id: worksheet[REPONSE_CODE_VALEUR + row].v,
+    CodeValeur: worksheet[REPONSE_CODE_VALEUR + row].v,
+    Libelle: worksheet[REPONSE_LIBELLE[level] + row].v,
+    Reponses
+  });
+
+  // Process until current row is empty
+  for (var row = START_ROW ; isValidRow(row) ; row++) {
+
+    if (isSection(row)) {
       // If current row as a section (not empty), make new section
 
       // Save the current section in questions
@@ -68,46 +89,24 @@ function transform(worksheet, callback) {
       }
 
       // Create a new section
-      section = {
-          id: id,
-          Section: worksheet[SECTION + row].v,
-          Libelle: worksheet[LIBELLE + row].v,
-          Trajectoire: worksheet[TRAJECTOIRE + row].v,
-          Question: worksheet[QUESTION + row].v,
-          Type: worksheet[TYPE + row].v,
-          Reponses: []
-        };
+      section = makeSection(row, id);
 
       // Create a new reponse
-      reponse = {
-        id: 0,
-        CodeValeur: '',
-        Libelle: worksheet[REPONSE_LIBELLE + row].v,
-        Reponses: []
-      };
+      reponse = makeReponse(worksheet, row, 0, []);
     } else {
       // Is not a Section but is ether a Reponse or SubReponse
-      if (worksheet[REPONSE_LIBELLE + row]) {
-        // This row is a Reponse
+
+      if (isResponseLevel(row, 1)) {
+        // This row is a Reponse level 2
 
         if (row > START_ROW) {
           section.Reponses.push(reponse);
         }
 
-        reponse = {
-          id: 0,
-          CodeValeur: '',
-          Libelle: worksheet[REPONSE_LIBELLE + row].v,
-          Reponses: []
-        };
-      } else if (worksheet[SUBREPONSE_LIBELLE + row]) {
-        // This row is a sub reponse
-        var subReponse = {
-          id: worksheet[REPONSE_CODE_VALEUR + row].v,
-          CodeValeur: worksheet[REPONSE_CODE_VALEUR + row].v,
-          Libelle: worksheet[SUBREPONSE_LIBELLE + row].v
-        };
-        reponse.Reponses.push(subReponse);
+        reponse = makeReponse(worksheet, row, 1, []);
+      } else if (isResponseLevel(row, 2)) {
+        var resp = makeReponse(worksheet, row, 2, []);
+        reponse.Reponses.push(resp);
       }
 
     }
