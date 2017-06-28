1f1b703f0fb8e21a34d74e4a74f70fe4bf46a521
Fabien Champigny <fabien@champigny.name>
2017-06-28 14:09:28 +0200

    Read first cell

--- scripts/load-geva-questions/index.js
+++ scripts/load-geva-questions/index.js
@@ -2,6 +2,8 @@
 
 var _ = require('lodash');
 var fs = require('fs');
+
+// https://github.com/SheetJS/js-xlsx
 var XLSX = require('xlsx');
 
 /*
@@ -26,7 +28,24 @@ function extract(xlsxFileName, callback) {
 // [Transform] : Format the list for the mongoDb
 function transform(workbook, callback) {
 
-  console.log('workbook', workbook);
+  var first_sheet_name = workbook.SheetNames[0];
+  var address_of_cell = 'A3';
+
+  /* Get worksheet */
+  var worksheet = workbook.Sheets[first_sheet_name];
+
+  /* Find desired cell */
+  var desired_cell = worksheet[address_of_cell];
+
+  /* Get the value */
+  var desired_value = (desired_cell ? desired_cell.v : undefined);
+
+  console.log('workbook', workbook.SheetNames);
+
+  console.log('desired_value', desired_value);
+
+
+  //console.log('workbook', workbook.Sheets[workbook.SheetNames[0]]);
 
   //callback('');
 };
@@ -35,7 +54,7 @@ function transform(workbook, callback) {
 function load(json) {
   // stringify: parameter '2' is for pretty output
   fs.writeFile(tmp_file, JSON.stringify(json, null, 2), function() {
-    console.log('You may load the mdphs list with :');
+    console.log('You may load the mdphs .list with :');
     console.log('mongoimport --db impact --collection mdphs --jsonArray --file tmp_mdphs.json');
   });
 }
