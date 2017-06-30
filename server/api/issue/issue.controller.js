2c0b5faf9916b4266316a827fb1444068177b1ab
Florian Delezenne <del.florian@gmail.com>
2017-06-30 11:50:54 +0200

    Ajout tests et affichage MDPH dans la liste des issues

--- server/api/issue/issue.controller.js
+++ server/api/issue/issue.controller.js
@@ -1,18 +1,32 @@
 import Issue from './issue.model';
+import User from '../user/user.model';
+import Promise from 'bluebird';
+
+function populateMdphs(issues) {
+  return Promise.map(issues, issue => {
+    return User
+      .populate(issue.user, 'mdph')
+      .then(populatedUser => {
+        issue.user = populatedUser;
+        return issue;
+      });
+  });
+}
 
 export function show(req, res) {
   return Issue
     .find({section: req.params.section})
     .populate('user')
     .exec()
-    .then(list => res.json(list))
+    .then(populateMdphs)
+    .then(res.json.bind(res))
     .catch(err => res.status(500).send(err.toString()));
 }
 
 export function create(req, res) {
   return Issue
     .create(req.body)
-    .then(created => res.json(created))
+    .then(res.json.bind(res))
     .catch(err => res.status(500).send(err.toString()));
 }
 
