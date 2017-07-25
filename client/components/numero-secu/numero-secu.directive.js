b8d72b30002527b73002383aaad46dde635bde5a
Florian Delezenne <del.florian@gmail.com>
2017-07-25 16:57:53 +0200

    Force value for numero securite social

---
+++ client/components/numero-secu/numero-secu.directive.js
@@ -0,0 +1,30 @@
+angular.module('impactApp').directive('numeroSecu', function (){
+  return {
+    require: 'ngModel',
+    link: function(scope, elem, attr, ngModel) {
+      const isValid = (value) => {
+        if (!value) return true;
+
+        const valueKey = value.slice(-2);
+        const valueRest = value.slice(0, -2);
+        const computedKey = 97 - valueRest % 97;
+
+        return parseInt(valueKey) == computedKey;
+      }
+
+      //For DOM -> model validation
+      ngModel.$parsers.unshift((value) => {
+        const valid = isValid(value);
+        ngModel.$setValidity('numero-secu', valid);
+        return valid;
+      });
+
+      //For model -> DOM validation
+      ngModel.$formatters.unshift((value) => {
+        const valid = isValid(value);
+        ngModel.$setValidity('numero-secu', valid);
+        return value;
+      });
+    }
+  };
+});
