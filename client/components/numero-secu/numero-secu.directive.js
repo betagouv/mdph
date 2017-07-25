dfa4303687f8e75c78467943461d0ca4bbdbd117
Florian Delezenne <del.florian@gmail.com>
2017-07-25 17:29:34 +0200

    Add values to e2e tests

--- client/components/numero-secu/numero-secu.directive.js
+++ client/components/numero-secu/numero-secu.directive.js
@@ -1,16 +1,20 @@
-angular.module('impactApp').directive('numeroSecu', function (){
+'use strict';
+
+angular.module('impactApp').directive('numeroSecu', function() {
   return {
     require: 'ngModel',
     link: function(scope, elem, attr, ngModel) {
       const isValid = (value) => {
-        if (!value) return true;
+        if (!value) {
+          return true;
+        }
 
         const valueKey = value.slice(-2);
         const valueRest = value.slice(0, -2);
         const computedKey = 97 - valueRest % 97;
 
-        return parseInt(valueKey) == computedKey;
-      }
+        return parseInt(valueKey) === computedKey;
+      };
 
       //For DOM -> model validation
       ngModel.$parsers.unshift((value) => {
