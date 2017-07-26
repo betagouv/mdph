b6560a0be0f4acd6cf137b36cd666215c2a0a3ab
lcommeau <lcommeau@norsys.fr>
2017-07-26 14:59:36 +0200

    correction lint

--- client/components/evaluation-profile-list/evaluation-profile-list.controller.js
+++ client/components/evaluation-profile-list/evaluation-profile-list.controller.js
@@ -2,9 +2,9 @@
 
 angular.module('impactApp').controller('EvaluationProfileListCtrl', function(MdphResource, Auth) {
 
-    this.profiles = MdphResource.queryBeneficiaires({zipcode: Auth.getCurrentUser().mdph.zipcode});
+  this.profiles = MdphResource.queryBeneficiaires({zipcode: Auth.getCurrentUser().mdph.zipcode});
 
-    this.getProfileTitle = (profile) => {
-      return `${profile.identites.beneficiaire.prenom} ${profile.identites.beneficiaire.nom}`;
-    };
+  this.getProfileTitle = (profile) => {
+    return `${profile.identites.beneficiaire.prenom} ${profile.identites.beneficiaire.nom}`;
+  };
 });
