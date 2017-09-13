18a9fbe8852c74f114cf8d39dc4649f595b61736
lcommeau <lcommeau@norsys.fr>
2017-09-13 10:46:09 +0200

    corrige test e2e

--- e2e/request/pages/beneficiaire.po.js
+++ e2e/request/pages/beneficiaire.po.js
@@ -11,18 +11,27 @@ var Beneficiaire = function() {
   form.surname = form.element(by.id('prenom'));
   form.radio_sexe = form.element(by.id('sexe-m'));
   form.radio_nat = form.element(by.id('nationalite_francaise'));
-  form.bday = form.element(by.id('date-de-naissance'));
+  form.birthday = form.element(by.id('date-de-naissance'));
+  form.birthTown = form.element(by.id('commune_naissance'));
+  form.birthDepartment = form.element(by.id('departement_naissance'));
+  form.birthCountry = form.element(by.id('pays_naissance'));
   form.address = form.element(by.id('address'));
   form.code_postal = form.element(by.id('code_postal'));
   form.localite = form.element(by.id('localite'));
+  form.email = form.element(by.id('email'));
   form.cpam = form.element(by.id('assurance_cpam'));
   form.numeroSecu = form.element(by.id('numero_secu'));
   form.submit = form.element(by.id('submit-benef'));
 
+
   this.submit = function() {
     form.name.sendKeys('test');
     form.surname.sendKeys('test');
+     form.email.sendKeys('test@mail.com');
     form.bday.sendKeys('01011900');
+    form.birthTown.sendKeys('007');
+    form.birthDepartment.sendKeys('75');
+    form.birthCountry.sendKeys('FRANCE');
     form.address.sendKeys('1, rue du test');
     form.code_postal.sendKeys('75001');
     form.localite.sendKeys('Paris');
