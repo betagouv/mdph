7454e11a78d3ab40157ab528205e38e60bb3c840
lcommeau <lcommeau@norsys.fr>
2017-10-05 11:39:24 +0200

    initialisation page acivation

--- client/app/departement/departement.controller.js
+++ client/app/departement/departement.controller.js
@@ -35,7 +35,7 @@ angular.module('impactApp')
         restrict: 'A',
         scope: {},
         link: function(scope, element, attr) {
-          element.attr('style', 'background-image : url(/api/mdphs/' + attr.mdphPhoto + '/photo/');
+          element.attr('style', 'background-image : url(/api/mdphs/' + attr.mdphPhoto + '/photo/)');
         }
       };
   });
