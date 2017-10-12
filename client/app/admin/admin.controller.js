90d2a94f648bac067a546010a7ee5535a5e17f26
lcommeau <lcommeau@norsys.fr>
2017-10-12 12:51:59 +0200

    suppression recherche MDPH pour layout admin

--- client/app/admin/admin.controller.js
+++ client/app/admin/admin.controller.js
@@ -5,5 +5,11 @@ angular.module('impactApp')
     this.getCurrentUser = Auth.getCurrentUser;
     this.isLoggedIn = Auth.isLoggedIn;
     this.logout = Auth.logout;
+
+    this.getCurrentMdphZipcode =  function() {
+      if (this.getCurrentUser().mdph) {
+        return this.getCurrentUser().mdph.zipcode;
+      }
+    };
   });
 
