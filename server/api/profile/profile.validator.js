5a0121190f4867fd1cf421dadc26431d5899d9ca
lcommeau <lcommeau@norsys.fr>
2017-10-17 16:56:35 +0200

    Add validator

---
+++ server/api/profile/profile.validator.js
@@ -0,0 +1,44 @@
+'use strict';
+
+import compose from 'composable-middleware';
+import Joi from 'joi';
+
+
+const beneficiaireSchema = Joi.object().keys({
+  localite: Joi.string().required(),
+  code_postal: Joi.string().required(),
+  nomVoie: Joi.string().required(),
+  dateNaissance: Joi.date().required(),
+  nationalite: Joi.string().required(),
+  sexe: Joi.string().required(),
+  prenom: Joi.string().required(),
+  nom: Joi.string().required(),
+  email: Joi.string().required(),
+  numero_secu: Joi.string().required(),
+  assurance: Joi.string().required()
+});
+
+export function check() {
+  return compose()
+    .use(function(req, res, next) {
+
+      if(req.body.identites.beneficiaire) {
+        Joi.validate(req.body.identites.beneficiaire, beneficiaireSchema, {allowUnknown: true}, (err, value) => {
+
+          var error = err.details.reduce(function(prev, curr) {
+            return [...prev, curr.message];
+          }, []);
+
+          if(err != null){
+             res.status(406).json(error);
+          }else{
+            next();
+          }
+        });
+      }else{
+        next();
+      }
+
+
+    });
+}
