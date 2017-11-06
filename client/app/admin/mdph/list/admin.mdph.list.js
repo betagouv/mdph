ec1474bdc33d0c9d2e7dde9743a5d036bbce8875
laurentcommeau <lcommeau@norsys.fr>
2017-11-06 15:02:56 +0100

    [EVOL] ETQ admin je souhaite avoir la liste des MDPH et le formulaire de modification sur des pages separées (#314)
    * separation des deux ecrans
    
    * correction transition vers le detail
    
    * correction lint

---
+++ client/app/admin/mdph/list/admin.mdph.list.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('impactApp')
+  .config(function($stateProvider) {
+    $stateProvider
+      .state('admin.mdph.list', {
+        url: '/mdph/list',
+        parent: 'admin',
+        authenticate: true,
+        templateUrl: 'app/admin/mdph/list/admin.mdph.list.html',
+        controller: 'AdminMdphListCtrl',
+        controllerAs: 'adminMdphListCtrl',
+        resolve: {
+          mdphs: function(MdphResource) {
+            return MdphResource.query().$promise;
+          }
+        }
+      });
+  });
