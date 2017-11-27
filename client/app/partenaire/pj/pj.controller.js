a5a3e5cd112e3c8b81451770ef1d815a827b80bf
lcommeau <lcommeau@norsys.fr>
2017-11-27 15:13:09 +0100

    Ajout de la verification de la signature pour les fichiers partenaire

--- client/app/partenaire/pj/pj.controller.js
+++ client/app/partenaire/pj/pj.controller.js
@@ -1,39 +1,48 @@
 'use strict';
 
 angular.module('impactApp')
-  .controller('PieceJointeCtrl', function($scope, $modal, Upload, $state, documentTypes, request, partenaire, currentMdph) {
+  .controller('PieceJointeCtrl', function($scope, $modal, Upload, $state, documentTypes, request, partenaire, currentMdph, FileSignatureService, toastr) {
     $scope.request = request;
     $scope.partenaire = partenaire;
     $scope.documentTypes = documentTypes;
     $scope.selectedType = documentTypes[0];
 
+    $scope.$on('file-upload-error', () => toastr.error('Types de documents acceptés : images (jpg, png) et pdf', 'Erreur lors de l\'envoi du document'));
+
     $scope.envoiConfirmation = function(form) {
       if (form.$valid) {
-        Upload.upload({
-          url: 'api/requests/' + $scope.request.shortId + '/document/partenaire',
-          method: 'POST',
-          data: {
-            file: $scope.files[0],
-            partenaire: partenaire._id,
-            type: $scope.selectedType.id
-          }
-        }).then(function() {
-          $modal.open({
-            templateUrl: 'app/partenaire/pj/confirmationModal.html',
-            controller: function($scope, $modalInstance, type) {
-              $scope.type = type;
-              $scope.ok = function() {
-                $modalInstance.close();
-                $state.go('layout', {codeDepartement: currentMdph.zipcode});
-              };
-            },
-
-            resolve: {
-              type: function() {
-                return $scope.selectedType.id;
+        FileSignatureService.check($scope.files[0], ['pdf', 'jpg', 'png'])
+        .then(function(result) {
+          if (result) {
+            Upload.upload({
+              url: 'api/requests/' + $scope.request.shortId + '/document/partenaire',
+              method: 'POST',
+              data: {
+                file: $scope.files[0],
+                partenaire: partenaire._id,
+                type: $scope.selectedType.id
               }
-            }
-          });
+            }).then(function() {
+              $modal.open({
+                templateUrl: 'app/partenaire/pj/confirmationModal.html',
+                controller: function($scope, $modalInstance, type) {
+                  $scope.type = type;
+                  $scope.ok = function() {
+                    $modalInstance.close();
+                    $state.go('layout', {codeDepartement: currentMdph.zipcode});
+                  };
+                },
+
+                resolve: {
+                  type: function() {
+                    return $scope.selectedType.id;
+                  }
+                }
+              });
+            });
+          } else {
+            $scope.$emit('file-upload-error', $scope.selectedType.id);
+          }
         });
       }
     };
