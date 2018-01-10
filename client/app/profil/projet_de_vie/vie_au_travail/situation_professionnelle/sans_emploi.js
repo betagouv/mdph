0f80feafcae94dc0e97497558efdf2df160a5cc2
lcommeau <lcommeau@norsys.fr>
2018-01-10 15:55:53 +0100

    Mantis 6882 : La question "Bénéficiez-vous d'une ou des prestations suivantes ?" est mal positionnée dans le questionnaire "Vie au travail"

--- client/app/profil/projet_de_vie/vie_au_travail/situation_professionnelle/sans_emploi.js
+++ client/app/profil/projet_de_vie/vie_au_travail/situation_professionnelle/sans_emploi.js
@@ -76,7 +76,7 @@ angular.module('impactApp')
           nextStep: function($state, saveCurrentState) {
             return function() {
               saveCurrentState();
-              $state.go('^.^.^.situation_professionnelle.rqth');
+              $state.go('^.^.^.situation_professionnelle.prestations');
             };
           }
         }
