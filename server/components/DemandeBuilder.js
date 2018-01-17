941aa34ecfbf423847bb951cb2954de766243ae7
lcommeau <lcommeau@norsys.fr>
2018-01-17 15:28:51 +0100

    correction demandBuilder

--- server/components/DemandeBuilder.js
+++ server/components/DemandeBuilder.js
@@ -110,6 +110,6 @@ function build({request, host, mdph, tempDirPath, withSeparator, format}) {
 export default function({request, host, mdph, withSeparator, format}) {
   var dirPromise = dir({unsafeCleanup: true, keep: true});
   return Promise.using(dirPromise, tempDirPath  => {
-    return build({tempDirPath, request, host, withSeparator, format});
+    return build({request, host, mdph, tempDirPath, withSeparator, format});
   });
 }
