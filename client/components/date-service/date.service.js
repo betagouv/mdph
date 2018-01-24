316d5122768a4e566f75ae884fa6f98bede6310c
asallem <aymen.sallem@gmail.com>
2018-01-24 15:31:38 +0100

    [Lot3] n°01 ETQ Bénéficiaire : Questionnaire "Identité bénéficiaire": évol champ "sécurité sociale" (#374)
    * [Lot3] n°01 ETQ Bénéficiaire : Questionnaire "Identité bénéficiaire": évol champ "sécurité sociale"
    
    * champ obligatoir doit être   en rouge en cas d'erreur apres validation

--- client/components/date-service/date.service.js
+++ client/components/date-service/date.service.js
@@ -1,6 +1,24 @@
 'use strict';
 
 angular.module('impactApp')
+  .factory('estAdulteStricte', function() {
+    return function(dateNaissance) {
+      if (dateNaissance) {
+        return moment().diff(dateNaissance, 'years') > 20;
+      }
+
+      return true;
+    };
+  })
+  .factory('estEnfant', function() {
+    return function(dateNaissance) {
+      if (dateNaissance) {
+        return moment().diff(dateNaissance, 'years') < 16;
+      }
+
+      return false;
+    };
+  })
   .factory('estAdulte', function() {
     return function(dateNaissance) {
       if (dateNaissance) {
