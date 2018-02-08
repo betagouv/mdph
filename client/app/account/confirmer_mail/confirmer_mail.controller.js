4847fa6d24127d4b8fbde6e7183aeef4d9a733fe
asallem <aymen.sallem@gmail.com>
2018-02-08 17:28:13 +0100

    la validation du nouveau compte de via le lien contenu dans l'email doit mèner l'usager à une page  indiquant que le compte a bel et bien été validé.

--- client/app/account/confirmer_mail/confirmer_mail.controller.js
+++ client/app/account/confirmer_mail/confirmer_mail.controller.js
@@ -3,12 +3,14 @@
 angular.module('impactApp')
   .controller('ConfirmerMailCtrl', function($scope, $state, $interval, $stateParams, $http) {
     $scope.pending = true;
-    $http.post('/api/users/' + $stateParams.userId + '/confirmer_mail/' + $stateParams.newMailToken).success(function() {
+    $http.post('/api/users/' + $stateParams.userId + '/confirmer_mail/' + $stateParams.newMailToken)
+    .then(function() {
       $scope.pending = false;
       $interval(function() {
         $state.go('login');
-      }, 5000, 1);
-    }).error(function() {
+      }, 10000, 1);
+    })
+    .catch(function() {
       $scope.pending = false;
       $scope.error = true;
     });
