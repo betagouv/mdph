4761035650b8fe9edffd76f0280410f9df664d8e
asallem <aymen.sallem@gmail.com>
2018-02-08 17:42:24 +0100

    Revert "la validation du nouveau compte de via le lien contenu dans l'email doit mèner l'usager à une page  indiquant que le compte a bel et bien été validé."
    This reverts commit 4847fa6d24127d4b8fbde6e7183aeef4d9a733fe.

--- client/app/account/confirmer_mail/confirmer_mail.controller.js
+++ client/app/account/confirmer_mail/confirmer_mail.controller.js
@@ -3,14 +3,12 @@
 angular.module('impactApp')
   .controller('ConfirmerMailCtrl', function($scope, $state, $interval, $stateParams, $http) {
     $scope.pending = true;
-    $http.post('/api/users/' + $stateParams.userId + '/confirmer_mail/' + $stateParams.newMailToken)
-    .then(function() {
+    $http.post('/api/users/' + $stateParams.userId + '/confirmer_mail/' + $stateParams.newMailToken).success(function() {
       $scope.pending = false;
       $interval(function() {
         $state.go('login');
-      }, 10000, 1);
-    })
-    .catch(function() {
+      }, 5000, 1);
+    }).error(function() {
       $scope.pending = false;
       $scope.error = true;
     });
