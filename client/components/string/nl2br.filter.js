7e5678629b2de6bcf43916fa17cf7435b58fd5a3
lcommeau <lcommeau@norsys.fr>
2018-02-13 16:08:26 +0100

    correction lint

--- client/components/string/nl2br.filter.js
+++ client/components/string/nl2br.filter.js
@@ -5,7 +5,7 @@ angular.module('impactApp')
     return function(msg,is_xhtml) {
       is_xhtml = is_xhtml || true;
       var breakTag = (is_xhtml) ? '<br />' : '<br>';
-      msg = (msg + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1'+ breakTag +'$2');
+      msg = (msg + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2');
       return $sce.trustAsHtml(msg);
     };
   });
