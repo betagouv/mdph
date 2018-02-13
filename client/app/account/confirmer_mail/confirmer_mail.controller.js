748a6e042af9a8c54d5e65996b29b64889315a51
asallem <aymen.sallem@gmail.com>
2018-02-13 17:07:29 +0100

    suppression du pending

--- client/app/account/confirmer_mail/confirmer_mail.controller.js
+++ client/app/account/confirmer_mail/confirmer_mail.controller.js
@@ -4,10 +4,8 @@ angular.module('impactApp')
   .controller('ConfirmerMailCtrl', function($scope, $state, $interval, $stateParams, $http, currentUser) {
     $scope.pending = true;
     $http.post('/api/users/' + $stateParams.userId + '/confirmer_mail/' + $stateParams.newMailToken).then(function() {
-      $scope.pending = false;
       currentUser.unconfirmed = false;
     }).catch(function() {
-      $scope.pending = false;
       $scope.error = true;
     });
   });
