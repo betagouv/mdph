b30671e50cdbec5ce056f7fa86a65279ba28cb19
asallem <aymen.sallem@gmail.com>
2018-02-13 15:12:52 +0100

    supprimer les commentaires et le pending

--- client/app/account/confirmer_mail/confirmer_mail.controller.js
+++ client/app/account/confirmer_mail/confirmer_mail.controller.js
@@ -6,9 +6,6 @@ angular.module('impactApp')
     $http.post('/api/users/' + $stateParams.userId + '/confirmer_mail/' + $stateParams.newMailToken).then(function() {
       $scope.pending = false;
       currentUser.unconfirmed = false;
-      /*$interval(function() {
-        $state.go('login');
-      }, 10000, 1);*/
     }).catch(function() {
       $scope.pending = false;
       $scope.error = true;
