a533d8b1aed63ef8af26709ae544ddd191a76fe0
laurentcommeau <lcommeau@norsys.fr>
2018-02-13 16:22:15 +0100

    Mantis 6908 : ETQ administrateur je souhaite saisir plusieurs ligne d'horaire (#434)
    * prise en compte du html pour les horaires d'une mdph dans le footer
    
    * utilisation d'un textarea pour la saisie du schedule des mdph
    
    * Ajout du filtre nl2br
    
    * coorection lint
    
    * correction lint

---
+++ client/components/string/nl2br.filter.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('impactApp')
+  .filter('nl2br', function($sce) {
+    return function(msg,is_xhtml) {
+      is_xhtml = is_xhtml || true;
+      var breakTag = (is_xhtml) ? '<br />' : '<br>';
+      msg = (msg + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2');
+      return $sce.trustAsHtml(msg);
+    };
+  });
