5e2d0e25455d9c72caf69dd99ad649894c3ba895
asallem <aymen.sallem@gmail.com>
2018-02-14 11:40:24 +0100

    Mantis 6957 & 6958 : Mantis 6957 : ETQ usagé je souhaiterais avoir le message "Votre compte a bien été validé" lorsque je clique sur le lien du mail de confirmation de compte et que mon compte est déjà activé.                                   & Mantis 6958 : ETQ usagé je souhaiterais que le bloc "Confirmer votre compte mail" ne soit plus visible après avoir validé mon compte (#437)
    * le lien de validation dans l'email mène l'usager à une page contenant le message "Votre compte a bien été validé".  Le lien mon compte permet d'acceder au compte en tant que utilisateur connecté.
    
    * supprimer les commentaires et le pending
    
    * ajouter lien vers profil
    
    * suppression du pending
    
    * corriger le cas de validation du compte apres déconnexion à l'application

--- client/app/account/confirmer_mail/confirmer_mail.controller.js
+++ client/app/account/confirmer_mail/confirmer_mail.controller.js
@@ -1,15 +1,12 @@
 'use strict';
 
 angular.module('impactApp')
-  .controller('ConfirmerMailCtrl', function($scope, $state, $interval, $stateParams, $http) {
-    $scope.pending = true;
-    $http.post('/api/users/' + $stateParams.userId + '/confirmer_mail/' + $stateParams.newMailToken).success(function() {
-      $scope.pending = false;
-      $interval(function() {
-        $state.go('login');
-      }, 5000, 1);
-    }).error(function() {
-      $scope.pending = false;
-      $scope.error = true;
+  .controller('ConfirmerMailCtrl', function($scope, $state, $interval, $stateParams, $http, currentUser) {
+    $http.post('/api/users/' + $stateParams.userId + '/confirmer_mail/' + $stateParams.newMailToken).then(function() {
+      currentUser.unconfirmed = false;
+    }).catch(function(res) {
+      if (res.status !== undefined) {
+        $scope.error = true;
+      }
     });
   });
