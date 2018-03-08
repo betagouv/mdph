54fde99b8ffd0c4a5d4b637c7b1956f991050d12
asallem <aymen.sallem@gmail.com>
2018-03-08 12:26:54 +0100

    corriger la gestion d'erreur dans le cas de modification des infos de l'agent

--- client/app/dashboard/users/agents/edit.controller.js
+++ client/app/dashboard/users/agents/edit.controller.js
@@ -29,28 +29,30 @@ angular.module('impactApp')
     };
 
     $scope.update = function(form) {
-      if ($scope.user._id) {
-        $scope.user.$changeInfo(function() {
-          $state.go('^', {}, {reload: true});
-        });
-      } else {
-        $scope.user.role = 'adminMdph';
-        $scope.user.mdph = currentMdph._id;
-        $scope.user.$saveAgent(
-          function() {
+      if (form.$valid) {
+        if ($scope.user._id) {
+          $scope.user.$changeInfo(function() {
             $state.go('^', {}, {reload: true});
-          },
-
-          function(err) {
-            err = err.data;
-            $scope.errors = {};
-
-            // Update validity of form fields that match the mongoose errors
-            angular.forEach(err.errors, function(error, field) {
-              form[field].$setValidity('mongoose', false);
-              $scope.errors[field] = error.message;
-            });
           });
+        } else {
+          $scope.user.role = 'adminMdph';
+          $scope.user.mdph = currentMdph._id;
+          $scope.user.$saveAgent(
+            function() {
+              $state.go('^', {}, {reload: true});
+            },
+
+            function(err) {
+              err = err.data;
+              $scope.errors = {};
+
+              // Update validity of form fields that match the mongoose errors
+              angular.forEach(err.errors, function(error, field) {
+                form[field].$setValidity('mongoose', false);
+                $scope.errors[field] = error.message;
+              });
+            });
+        }
       }
     };
 
