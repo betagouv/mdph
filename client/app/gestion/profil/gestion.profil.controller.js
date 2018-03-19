9f4e8fbb5b1924e19543af566fff37cc59dc871d
lcommeau <lcommeau@norsys.fr>
2018-03-19 17:13:41 +0100

    creation page

--- client/app/gestion/profil/gestion.profil.controller.js
+++ client/app/gestion/profil/gestion.profil.controller.js
@@ -1,7 +1,23 @@
 'use strict';
 
 angular.module('impactApp')
-  .controller('GestionProfilCtrl', function($state, currentMdph) {
+  .controller('GestionProfilCtrl', function($state, currentMdph, currentUser, profils, ProfileResource) {
     this.currentMdph = currentMdph;
+    this.currentUser = currentUser;
+    this.profils = profils;
+    this.deletedProfils = profils;
 
+    this.createProfil = function() {
+      new ProfileResource().$save({userId: this.currentUser._id}, function(result) {
+        $state.go('profil', {profileId: result._id});
+      });
+    };
+
+    this.deleteProfil = function() {
+
+    };
+
+    this.goProfil = function(profil) {
+      $state.go('gestion_demande', {profilId: profil._id});
+    };
   });
