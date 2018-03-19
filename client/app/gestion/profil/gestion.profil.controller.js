89e20c64fd0b8001f32a6be9e6c5c98609cb1545
lcommeau <lcommeau@norsys.fr>
2018-03-19 09:23:14 +0100

    suppression  du routage vers gestion

--- client/app/gestion/profil/gestion.profil.controller.js
+++ client/app/gestion/profil/gestion.profil.controller.js
@@ -4,10 +4,4 @@ angular.module('impactApp')
   .controller('GestionProfilCtrl', function($state, currentMdph, profils) {
     this.currentMdph = currentMdph;
 
-    this.init = () => {
-      if (profils.length === 1) {
-        return $state.go('gestion_demande', {profilId: profils[0]._id}, {reload: true});
-      }
-    };
-
   });
