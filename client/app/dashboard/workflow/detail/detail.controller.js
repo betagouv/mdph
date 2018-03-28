63d8a7bec92a9f04a48c02ad93b46c43662c3370
Abdelaziz BOUSSABAT <aboussabat@norsys.fr>
2018-03-28 13:17:36 +0200

    Le clic sur le bouton « Supprimer » fait apparaitre la pop-in  (validee et irrecevable)

--- client/app/dashboard/workflow/detail/detail.controller.js
+++ client/app/dashboard/workflow/detail/detail.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('impactApp')
-  .controller('WorkflowDetailCtrl', function($scope, $state, $cookies, request) {
+  .controller('WorkflowDetailCtrl', function($scope, $state, $cookies, $modal, request) {
     $scope.request = request;
     $scope.token = $cookies.get('token');
 
@@ -11,5 +11,27 @@ angular.module('impactApp')
 
     $scope.showRemoveButton = function() {
       $scope.removeButtonShowed = true;
+    };
+
+    $scope.openModal = function() {
+      let request = $scope.request;
+      let token = $scope.token;
+
+      $modal.open({
+        templateUrl: 'app/dashboard/workflow/detail/modal.html',
+        controllerAs: 'modalRemove',
+        size: 'md',
+        controller($modalInstance, $state, RequestService) {
+          this.shortId = request.shortId;
+          this.remove = function() {
+            console.info('remove');
+            //TODO service de suppression des demandes
+          };
+
+          this.cancel = function() {
+            $modalInstance.dismiss('cancel');
+          };
+        }
+      });
     };
   });
