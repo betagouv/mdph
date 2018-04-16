025edd1689f22d37ee56f225e3463d088758b27c
Abdelaziz BOUSSABAT <aboussabat@norsys.fr>
2018-04-16 10:34:17 +0200

    Correction lint

--- client/app/dashboard/workflow/workflow.controller.js
+++ client/app/dashboard/workflow/workflow.controller.js
@@ -3,7 +3,6 @@
 angular.module('impactApp')
   .controller('WorkflowCtrl', function($scope, $modal, $window, $cookies, $state, $http, RequestResource) {
     this.token = $cookies.get('token');
-c
     this.openDeleteModal = function(request) {
       $modal.open({
         templateUrl: 'app/dashboard/workflow/detail/modalDelete.html',
@@ -13,19 +12,18 @@ c
           this.shortId = request.shortId;
 
           this.delete = function() {
-            //TODO appeler le service de suppression
-            //$http.delete('/api/requests/' + this.shortId).then(() => {
+              //TODO appeler le service de suppression
+              //$http.delete('/api/requests/' + this.shortId).then(() => {
               $modalInstance.close();
-              if($state.includes('dashboard.workflow.detail')){
-                console.info('detail');
+              if ($state.includes('dashboard.workflow.detail')) {
                 //TODO mettre les bons parms apres l'integration de la fiche sur le menu courant
                 $state.go('dashboard.workflow.list', {userId:'me', status:'emise'}, {reload: true});
               } else {
-                console.info('list');
                 $state.go('.', {}, {reload: true});
               }
-           // });
-          };
+
+              // });
+            };
 
           this.cancel = function() {
             $modalInstance.dismiss('cancel');
