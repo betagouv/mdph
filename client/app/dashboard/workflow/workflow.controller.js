8461d28254fd0d4ef8c06e37c93145f1d0bcf2d2
Abdelaziz BOUSSABAT <aboussabat@norsys.fr>
2018-04-16 10:15:37 +0200

    redirige l'agent vers le tableau de bord

--- client/app/dashboard/workflow/workflow.controller.js
+++ client/app/dashboard/workflow/workflow.controller.js
@@ -1,9 +1,9 @@
 'use strict';
 
 angular.module('impactApp')
-  .controller('WorkflowCtrl', function($scope, $modal, $window, $cookies, RequestResource) {
+  .controller('WorkflowCtrl', function($scope, $modal, $window, $cookies, $state, $http, RequestResource) {
     this.token = $cookies.get('token');
-
+c
     this.openDeleteModal = function(request) {
       $modal.open({
         templateUrl: 'app/dashboard/workflow/detail/modalDelete.html',
@@ -13,7 +13,18 @@ angular.module('impactApp')
           this.shortId = request.shortId;
 
           this.delete = function() {
-            //TODO service de suppression des demandes
+            //TODO appeler le service de suppression
+            //$http.delete('/api/requests/' + this.shortId).then(() => {
+              $modalInstance.close();
+              if($state.includes('dashboard.workflow.detail')){
+                console.info('detail');
+                //TODO mettre les bons parms apres l'integration de la fiche sur le menu courant
+                $state.go('dashboard.workflow.list', {userId:'me', status:'emise'}, {reload: true});
+              } else {
+                console.info('list');
+                $state.go('.', {}, {reload: true});
+              }
+           // });
           };
 
           this.cancel = function() {
