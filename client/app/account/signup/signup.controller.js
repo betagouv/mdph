8131ea925507fe8e13c63c5b6ebcd75c1bd462f2
asallem <aymen.sallem@gmail.com>
2018-04-17 16:16:29 +0200

    appliquer HoneyPot à la page d'inscription (#463)

--- client/app/account/signup/signup.controller.js
+++ client/app/account/signup/signup.controller.js
@@ -4,6 +4,8 @@ angular.module('impactApp')
   .controller('SignupCtrl', function($rootScope, $scope, $state, ProfileResource, Auth, currentMdph) {
     $scope.user = {};
     $scope.inputType = 'password';
+    $scope.show = false;
+    $scope.startTime =  $scope.startTime ? $scope.startTime : new Date().getTime();
 
     $scope.toggleType = function() {
       if ($scope.inputType === 'password') {
@@ -18,26 +20,35 @@ angular.module('impactApp')
     };
 
     $scope.register = function(form) {
-      if (form.$valid) {
-        Auth.createUser({
-          email: form.email.$modelValue,
-          password: form.password.$modelValue,
-          mdph: currentMdph.zipcode
-        })
-        .then(function(data) {
-          // Logged in, redirect
-          return $state.go('demande.beneficiaire', {profileId: data.profile}, {reload: true});
-        })
-        .catch(function(err) {
-          err = err.data;
-          $scope.errors = {};
+      let delaisMin = 1000;
+      if (form.contact.$modelValue === '' && form.emailSecours.$modelValue === 'your@email.com') {
+        if ((new Date().getTime() - $scope.startTime) >  delaisMin) {
+          if (form.$valid) {
+            Auth.createUser({
+              email: form.email.$modelValue,
+              password: form.password.$modelValue,
+              mdph: currentMdph.zipcode
+            })
+            .then(function(data) {
+              // Logged in, redirect
+              return $state.go('profil.beneficiaire', {profileId: data.profile}, {reload: true});
+            })
+            .catch(function(err) {
+              err = err.data;
+              $scope.errors = {};
 
-          // Update validity of form fields that match the mongoose errors
-          angular.forEach(err.errors, function(error, field) {
-            form[field].$setValidity('mongoose', false);
-            $scope.errors[field] = error.message;
-          });
-        });
+              // Update validity of form fields that match the mongoose errors
+              angular.forEach(err.errors, function(error, field) {
+                form[field].$setValidity('mongoose', false);
+                $scope.errors[field] = error.message;
+              });
+            });
+          }
+        }  else {
+          form.emailSecours.$setValidity('formulaire', false);
+        }
+      } else {
+        form.contact.$setValidity('formulaire', false);
       }
     };
   });
