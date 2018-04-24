83109981b688daa8dd899723d95ee1eeae17df60
Abdelaziz BOUSSABAT <aboussabat@norsys.fr>
2018-04-24 17:54:56 +0200

    Suppression de champs de saisie de l’email

--- client/app/closed/closed.controller.js
+++ client/app/closed/closed.controller.js
@@ -4,18 +4,4 @@ angular.module('impactApp')
   .controller('ClosedCtrl', function($http, $timeout, currentMdph) {
     this.currentMdph = currentMdph;
 
-    this.submit = () => {
-      this.loading = true;
-      this.thankyou = false;
-
-      $http.post(`/api/mdphs/${currentMdph.zipcode}/like`, {email: this.subscribeEmail})
-        .then(this.showThanks);
-    };
-
-    this.showThanks = () => {
-      $timeout(() => {
-        this.loading = false;
-        this.thankyou = true;
-      }, 1000);
-    };
   });
