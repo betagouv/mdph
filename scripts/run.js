785328094cf5b1b12ee2a61878150beda036a26a
lcommeau <lcommeau@norsys.fr>
2018-05-24 16:44:13 +0200

    initialisation script migration lot4

--- scripts/run.js
+++ scripts/run.js
@@ -5,7 +5,7 @@ require('babel-register');
 const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 
-mongoose.connect('mongodb://localhost/test_prod', {useMongoClient: true, poolSize: 2});
+mongoose.connect('mongodb://localhost/impact_prod', {useMongoClient: true, poolSize: 2});
 
 //require('./default_profiles.js');
-require('./synthese-delete-profil.js');
+require('./migration-lot4');
